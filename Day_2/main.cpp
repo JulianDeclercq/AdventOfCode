d5a65367293819455c48f9b09c5e63d777065af6
Julian_Declercq <julian.declercq@student.howest.be>
2016-12-02 20:23:47 +0100

    Changed code structure of day 2 so a simple bool trigger switches to part 1 or part 2.

--- Day_2/main.cpp
+++ Day_2/main.cpp
@@ -8,51 +8,40 @@ std::vector<std::string> commands;
 std::map<char, int> actions;
 int curNr = 5;
 
+bool part1 = false;
+
 void LoadActions()
 {
-	/*
-		1 2 3
-		4 5 6
-		7 8 9
-	*/
-	actions.insert(std::make_pair('U', -3));
-	actions.insert(std::make_pair('R', +1));
-	actions.insert(std::make_pair('D', +3));
-	actions.insert(std::make_pair('L', -1));
-}
-
-void LoadActions2()
-{
-	/*
-		1
-	  2 3 4
-	5 6 7 8 9
-	  A B C
-		D
-	*/
-	actions.insert(std::make_pair('U', -4)); // Regular UP
-	actions.insert(std::make_pair('R', +1));
-	actions.insert(std::make_pair('D', +4)); // Regular DOWN
-	actions.insert(std::make_pair('L', -1));
-}
-
-bool CommandValid(const char c)
-{
-	switch (c)
+	if (part1)
 	{
-	case 'U': return (curNr > 3);
-	case 'R': return (curNr % 3 != 0);
-	case 'D': return (curNr < 7);
-	case 'L': return (curNr % 3 != 1);
+		/*
+			1 2 3
+			4 5 6
+			7 8 9
+		*/
+		actions.insert(std::make_pair('U', -3));
+		actions.insert(std::make_pair('R', +1));
+		actions.insert(std::make_pair('D', +3));
+		actions.insert(std::make_pair('L', -1));
 	}
-
-	std::cout << "Something went wrong in CommandValid.\n" << std::endl;
-	return false;
+	else
+	{
+		/*
+			1
+		  2 3 4
+		5 6 7 8 9
+		  A B C
+			D
+		*/
+		actions.insert(std::make_pair('U', -4)); // Regular UP
+		actions.insert(std::make_pair('R', +1));
+		actions.insert(std::make_pair('D', +4)); // Regular DOWN
+		actions.insert(std::make_pair('L', -1));
+	}
 }
 
 bool CommandValid2(const char c, bool& specialCmd)
 {
-	// Have to be declared before switch statement
 	std::vector<int> v = std::vector<int>();
 	std::vector<int>::iterator it = std::vector<int>::iterator();
 
@@ -74,25 +63,52 @@ bool CommandValid2(const char c, bool& specialCmd)
 	return (it == v.end());
 }
 
+bool CommandValid(const char c, bool& specialCmd)
+{
+	if (part1)
+	{
+		switch (c)
+		{
+		case 'U': return (curNr > 3);
+		case 'R': return (curNr % 3 != 0);
+		case 'D': return (curNr < 7);
+		case 'L': return (curNr % 3 != 1);
+		}
+	}
+
+	// If part 2
+	return CommandValid2(c, specialCmd);
+}
+
 void ExecuteCommandLine(const std::string& cmd)
 {
 	for (const char& c : cmd)
 	{
-		//if (CommandValid(c))
 		bool specialCmd = false;
-		if (CommandValid2(c, specialCmd))
+		if (part1)
 		{
-			curNr += (specialCmd) ? actions.at(c) / 2 : actions.at(c);
+			if (CommandValid(c, specialCmd))
+			{
+				curNr += actions.at(c);
+			}
+		}
+		else
+		{
+			if (CommandValid2(c, specialCmd))
+			{
+				curNr += (specialCmd) ? actions.at(c) / 2 : actions.at(c);
+			}
 		}
 	}
 
 	// Translating numbers higher than 10 to A B C D
 	char answer = (curNr > 10) ? static_cast<char>(curNr % 10 + 'A') : static_cast<char>(curNr + '0');
-	std::cout << "Number: " << answer << std::endl;
+	std::cout << answer;
 }
 
 int main()
 {
+	// File reading
 	std::ifstream file("input.txt");
 	if (file.fail())
 	{
@@ -107,13 +123,13 @@ int main()
 	}
 	file.close();
 
-	// Part 1
-	//LoadActions();
-	LoadActions2();
+	// Executing
+	LoadActions();
 	for (const std::string& line : commands)
 	{
 		ExecuteCommandLine(line);
 	}
+	std::cout << std::endl;
 
 	return 0;
 }

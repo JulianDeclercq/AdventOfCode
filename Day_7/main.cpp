1ad4d698bb0b71b66d0c2be3f9ed966f78adcf90
Julian_Declercq <julian.declercq@student.howest.be>
2016-12-12 17:45:35 +0100

    Completed day 7 part 1.

--- Day_7/main.cpp
+++ Day_7/main.cpp
@@ -4,9 +4,18 @@
 #include <fstream>
 #include <algorithm>
 
-bool HasABBA(const std::string& str)
+bool OneCharString(const std::string& s)
 {
-	std::string cpy(str.substr(1, str.size() - 2));
+	return s.find_first_not_of(s[0]) == std::string::npos;
+}
+
+bool CheckABBA(const std::string& str, bool encl)
+{
+	std::string cpy(str);
+	if (encl) // if string was an enclosed string, get the brackets out
+	{
+		cpy = (str.substr(1, str.size() - 2));
+	}
 	// Break whole string in 4sized strings
 	std::vector<std::string> parts;
 	for (size_t i = 0; i < cpy.size() - 3; ++i)
@@ -19,7 +28,7 @@ bool HasABBA(const std::string& str)
 		const std::string sub = part.substr(0, 2);
 		std::string rev(part.substr(2, 2));
 		std::reverse(rev.begin(), rev.end());
-		if (sub.compare(rev) == 0)
+		if (sub.compare(rev) == 0 && !OneCharString(sub))
 		{
 			return true;
 		}
@@ -27,10 +36,54 @@ bool HasABBA(const std::string& str)
 	return false;
 }
 
+void ExtractParts(const std::string& str, std::vector<std::string>& parts, std::vector<std::string>& enclParts)
+{
+	size_t findPos = 0, closePos = 0;
+	findPos = str.substr(closePos).find('[');
+
+	// Recursion ending condition
+	if (findPos == str.npos)
+	{
+		parts.push_back(str);
+		return;
+	}
+
+	closePos = str.find(']') + 1; // no check if invalid, assume every '[' has a closing ']'
+
+	parts.push_back(str.substr(0, findPos));
+	enclParts.push_back(str.substr(findPos, (closePos - findPos)));
+
+	ExtractParts(str.substr(closePos), parts, enclParts);
+}
+
+bool ValidIPv7(const std::vector<std::string>& parts, const std::vector<std::string>& enclParts)
+{
+	bool hasABBA = false, failedABBA = false;
+
+	for (const std::string& part : parts)
+	{
+		if (CheckABBA(part, false))
+		{
+			hasABBA = true;
+			break;
+		}
+	}
+
+	for (const std::string& part : enclParts)
+	{
+		if (CheckABBA(part, true))
+		{
+			failedABBA = true;
+			break;
+		}
+	}
+	return(hasABBA && (!failedABBA));
+}
+
 int main()
 {
-	//	std::ifstream file("input.txt");
-	std::ifstream file("example.txt");
+	std::ifstream file("input.txt");
+	//std::ifstream file("example.txt");
 
 	if (file.fail())
 	{
@@ -38,40 +91,27 @@ int main()
 		return -1;
 	}
 
-	std::string line;
+	std::string line, answer;
+	int validCtr = 0;
 	while (std::getline(file, line))
 	{
-		std::vector<std::string> enclosedParts, nonenclosedParts;
-		size_t findPos = 0, closePos = 0;
-		bool registeredFirst = false;
+		std::vector<std::string> parts, enclParts;
 		std::string cpy(line);
 
-		do
+		ExtractParts(cpy, parts, enclParts);
+
+		if (ValidIPv7(parts, enclParts))
 		{
-			findPos = cpy.substr(closePos).find('[');
-			if (!registeredFirst)
-			{
-				nonenclosedParts.push_back(cpy.substr(0, findPos));
-				registeredFirst = true;
-			}
-			if (findPos == std::string::npos)
-			{
-				break;
-			}
-			closePos = cpy.find(']') + 1; // no check if invalid, assume every '[' has a closing ']', add 1 so it is included in the next line's substr
-
-			std::string nonecnl = cpy.substr(closePos, findPos);
-			nonenclosedParts.push_back(nonecnl);
-
-			enclosedParts.push_back(cpy.substr(findPos, (closePos - findPos)));
-		} while (findPos != std::string::npos);
-
-		for (const std::string& part : enclosedParts)
-		{
-			HasABBA(part);
+			//std::cout << "Valid line: " << cpy << std::endl;
+			++validCtr;
 		}
-		std::cout << "LINE\n";
+		else
+		{
+			//std::cout << "Invalid line: " << cpy << std::endl;
+		}
 	}
+
+	std::cout << validCtr << " valid IPv7 addresses\n";
 
 	file.close();
 

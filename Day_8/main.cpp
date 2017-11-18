3fea658146f9829d01230e81bfd3d621465286a2
Julian Declercq <julian.declercq1@gmail.com>
2017-11-18 18:49:59 +0100

    Implemented Day 8 part 1. All functionality. Examples working.

--- Day_8/main.cpp
+++ Day_8/main.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <array>
 #include <fstream>
 #include <regex>
 
 using namespace std;
 
-void CreateRect(int x, int y)
-{
-}
-
-void RotateColumn(int columnNumber, int amount)
-{
-}
-
-void RotateRow(int rowNumber, int amount)
-{
-}
+const int GRID_WIDTH = 50, GRID_HEIGHT = 6;
+//const int GRID_WIDTH = 7, GRID_HEIGHT = 3;
+array<bool, GRID_WIDTH * GRID_HEIGHT> Lights = array<bool, GRID_WIDTH * GRID_HEIGHT>();
 
 enum CommandType
 {
@@ -24,6 +16,81 @@ enum CommandType
 	ROTATE_COLUMN,
 	ROTATE_ROW
 };
+
+void CreateRect(int x, int y)
+{
+	// Command "rect X*Y" turns on all of the pixels in a rectangle at the top-left of the screen which is X wide and Y tall.
+	for (int column = 0; column < x; ++column)
+	{
+		for (int row = 0; row < y; ++row)
+		{
+			Lights[GRID_WIDTH * row + column] = true;
+		}
+	}
+}
+
+// Shifts a column once
+void ShiftColumn(int columnNumber)
+{
+	// Save the state before shifting
+	array<bool, GRID_HEIGHT> startState = array<bool, GRID_HEIGHT>();
+	for (int i = 0; i < GRID_HEIGHT; ++i)
+	{
+		startState[i] = Lights[columnNumber + (i * GRID_WIDTH)];
+	}
+
+	// Now that they have all been saved, set the next one to the previous one.
+	for (int i = 0; i < GRID_HEIGHT; ++i)
+	{
+		if (i == GRID_HEIGHT - 1)
+		{
+			Lights[columnNumber] = startState[i];
+		}
+		else
+		{
+			Lights[columnNumber + ((i + 1) * GRID_WIDTH)] = startState[i];
+		}
+	}
+}
+
+void RotateColumn(int columnNumber, int amount)
+{
+	for (int i = 0; i < amount; ++i)
+	{
+		ShiftColumn(columnNumber);
+	}
+}
+
+// Shifts a row once
+void ShiftRow(int rowNumber)
+{
+	// Save the state before shifting
+	array<bool, GRID_WIDTH> startState = array<bool, GRID_WIDTH>();
+	for (int i = 0; i < GRID_WIDTH; ++i)
+	{
+		startState[i] = Lights[GRID_WIDTH * rowNumber + i];
+	}
+
+	// Now that they have all been saved, set the next one to the previous one.
+	// Important to do this in a next loop otherwise all numbers would be the same
+	for (int i = 0; i < GRID_WIDTH; ++i)
+	{
+		// Calculate the index. If I is the last in the row, idx should be reset in order to loop
+		size_t idx = (i == GRID_WIDTH - 1) ? GRID_WIDTH * rowNumber : GRID_WIDTH * rowNumber + i + 1;
+
+		// Update the light
+		Lights[idx] = startState[i];
+	}
+}
+
+void RotateRow(int rowNumber, int amount)
+{
+	// Rotate row y = A by B shifts all of the pixels in row A(0 is the top row) right by B pixels. Pixels that would fall off the right end appear at the left end of the row.
+	for (int i = 0; i < amount; ++i)
+	{
+		ShiftRow(rowNumber);
+	}
+}
 
 CommandType DetermineCommandType(const string& command)
 {
@@ -86,16 +153,41 @@ void ProcessCommand(const string& command)
 	}
 }
 
+void PrintMonitor()
+{
+	// Print the current state of the lights
+	for (int i = 0; i < Lights.size(); ++i)
+	{
+		// Take a new line for each row
+		if (i != 0 && i % GRID_WIDTH == 0)
+		{
+			cout << endl;
+		}
+
+		cout << ((Lights[i]) ? '#' : '.');
+	}
+	cout << endl << endl;
+}
+
 int main()
 {
-	//DEBUG
-
-	//ProcessCommand("rect 195x3932");
-	ProcessCommand("rotate column x=30 by 1");
-	return 0;
+	// Example
+/*
+	ProcessCommand("rect 3x2");
+	PrintMonitor();
+	ProcessCommand("rotate column x=1 by 1");
+	PrintMonitor();
+	ProcessCommand("rotate row y=0 by 4");
+	PrintMonitor();
+	ProcessCommand("rotate column x=1 by 1");
+	PrintMonitor();
+	ProcessCommand("rotate column x=1 by 4");
+	PrintMonitor();
+	cin.get();
+	return 0;*/
 
 	// Open the inputfile
-	ifstream input("example.txt");
+	ifstream input("input.txt");
 
 	// Error check for the input file
 	if (input.fail())
@@ -109,7 +201,17 @@ int main()
 	while (getline(input, line))
 	{
 		ProcessCommand(line);
+		cout << line << endl;
+		PrintMonitor();
 	}
+
+	int activeCtr = 0;
+	for (bool b : Lights)
+		if (b) ++activeCtr;
+
+	cout << "There are " << activeCtr << " lights active.\n";
+
+	cin.get();
 
 	return 0;
 }

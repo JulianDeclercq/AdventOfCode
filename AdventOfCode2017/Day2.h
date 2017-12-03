7750a8b174f44d9f3b927a7565b9483ad3def5d8
Julian Declercq <julian.declercq1@gmail.com>
2017-12-04 00:59:32 +0100

    Cleaned repository. Properly separated definitions from implementations  with .h and .cpp files.

--- AdventOfCode2017/Day2.h
+++ AdventOfCode2017/Day2.h
@@ -8,10 +8,9 @@
 
 using namespace std;
 
-namespace Day2
+class Day2
 {
-#pragma region General
-
+private:
 	vector<string> Rows = vector<string>();
 
 	template<typename T>
@@ -23,91 +22,16 @@ namespace Day2
 			*(result++) = stoi(item);
 	}
 
-	vector<int> split(const string &s, char delim)
-	{
-		vector<int> elems;
-		split(s, delim, std::back_inserter(elems));
-		return elems;
-	}
+	vector<int> split(const string &s, char delim);
 
-	void ParseInput()
-	{
-		//ifstream input("Example/Day2Part1.txt");
-		//ifstream input("Example/Day2Part2.txt");
-		ifstream input("Input/Day2.txt");
-		if (input.fail())
-		{
-			std::cout << "Failed to open input file.\n";
-			return;
-		}
+	// Part 1
+	void ParseInput();
+	int RowDifference(const string& row);
 
-		string line;
-		while (getline(input, line))
-			Rows.push_back(line);
-	}
-#pragma endregion
+	// Part 2
+	int EvenlyDivided(const string& row);
 
-#pragma region Part1
-
-	int RowDifference(const string& row)
-	{
-		// Parse the row string to a vector of integers
-		vector<int> numbers = split(row, ' ');
-		int maxElement = *max_element(numbers.begin(), numbers.end());
-		int minElement = *min_element(numbers.begin(), numbers.end());
-		return maxElement - minElement;
-	}
-
-	void Part1()
-	{
-		ParseInput();
-
-		int checksum = 0;
-		for (const std::string& row : Rows)
-			checksum += RowDifference(row);
-
-		std::cout << "Day 2 Part 1 answer is: " << checksum << std::endl;
-	}
-#pragma endregion
-
-#pragma region Part2
-	int EvenlyDivided(const string& row)
-	{
-		/*		Find the only two numbers in each row where one evenly divides the other
-		so where the result of the division operation is a whole number.		*/
-
-		// Parse the row string to a vector of integers
-		vector<int> numbers = split(row, ' ');
-		for (int number : numbers)
-		{
-			for (int number2 : numbers)
-			{
-				// Ignore checking with the same numbers because of nested loop
-				// Ignore checking when first number is smaller than second number
-				if (number == number2 || number < number2)
-					continue;
-
-				float division = static_cast<float>(number) / static_cast<float>(number2);
-
-				// Check if it is a whole number and return it if it is
-				if (floor(division) == division)
-					return static_cast<int>(division);
-			}
-		}
-
-		cout << "Error, no numbers where division is a whole number exist.\n";
-		return -1;
-	}
-
-	void Part2()
-	{
-		ParseInput();
-
-		int checksum = 0;
-		for (const std::string& row : Rows)
-			checksum += EvenlyDivided(row);
-
-		std::cout << "Day 2 Part 2 answer is: " << checksum << std::endl;
-	}
-#pragma endregion
-}
+public:
+	void Part1();
+	void Part2();
+};
--- AdventOfCode2017/Day3.h
+++ AdventOfCode2017/Day3.h
@@ -6,148 +6,33 @@
 
 using namespace std;
 
-namespace Day3
+struct Point
 {
-#pragma region General
-	struct Point
-	{
-		Point() {}
-		Point(int x, int y) : X(x), Y(y) {}
-		int X = 0, Y = 0;
-	};
+	Point() {}
+	Point(int x, int y) : X(x), Y(y) {}
+	int X = 0, Y = 0;
+};
 
-	bool operator== (const Point &p1, const Point &p2)
-	{
-		return (p1.X == p2.X && p1.Y == p2.Y);
-	}
-
-	bool operator< (const Point &p1, const Point &p2)
-	{
-		return tie(p1.X, p1.Y) < tie(p2.X, p2.Y);
-	}
+class Day3
+{
+private:
 
 	// The puzzle input
 	const int _puzzleInput = 361527;
 
 	// A map containing all numbers and their respective positions
-	map<int, Point> _numberPositions = map<int, Point>();
-	map<Point, int> _positionNumbers = map<Point, int>();
+	map<int, Point> _numberPositions = map<int, Point>(); // part 1
+	map<Point, int> _positionNumbers = map<Point, int>(); // part 2
 
-	int ManhattanDistance(Point p1, Point p2)
-	{
-		return (abs(p1.X - p2.X) + abs(p1.Y - p2.Y));
-	}
+	// Part 1
+	int ManhattanDistance(Point p1, Point p2);
+	void GenerateSpiralMemory();
 
-	void GenerateSpiralMemory()
-	{
-		int step = 1, stepProgression = 0, xCoord = 0, yCoord = 0;
-		bool horizontal = true;
-		int direction = 1; // (Horizontal: left <-> right. Vertical: up <-> down)
-		for (int i = 1; i < _puzzleInput + 1; ++i)
-		{
-			// Add the current number on the current position
-			_numberPositions.insert({ i, Point(xCoord, yCoord) });
+	// Part 2
+	int NeighbourValueSum(Point source);
+	void GenerateSpiralMemory2();
 
-			// Calculate the next numbers position
-			(horizontal) ? xCoord += direction : yCoord += direction;
-
-			// Increment step progression
-			++stepProgression;
-
-			// If this step has now been completed,
-			if (stepProgression == step)
-			{
-				// Flip the direction
-				horizontal = !horizontal;
-
-				// Reset the step progression
-				stepProgression = 0;
-
-				// When switched back from vertical to horizontal:
-				// * the step should be incremented
-				// * the direction changes (Horizontal: left <-> right. Vertical: up <-> down)
-				if (horizontal)
-				{
-					++step;
-					direction *= -1;
-				}
-			}
-		}
-	}
-
-	int NeighbourValueSum(Point source)
-	{
-		return _positionNumbers[Point(source.X - 1, source.Y + 1)] + _positionNumbers[Point(source.X, source.Y + 1)] + _positionNumbers[Point(source.X + 1, source.Y + 1)] + // top row neighbors
-			_positionNumbers[Point(source.X - 1, source.Y)] + _positionNumbers[Point(source.X + 1, source.Y)] + // same row neighbors
-			_positionNumbers[Point(source.X - 1, source.Y - 1)] + _positionNumbers[Point(source.X, source.Y - 1)] + _positionNumbers[Point(source.X + 1, source.Y - 1)]; // bottom row neighbors
-	}
-
-	void GenerateSpiralMemory2()
-	{
-		int step = 1, stepProgression = 0, xCoord = 0, yCoord = 0;
-		bool horizontal = true;
-		int direction = 1; // (Horizontal: left <-> right. Vertical: up <-> down)
-
-		// Infinite loop. Is broken as soon as an answer to the puzzle is found
-		for (;;)
-		{
-			// Add the current number on the current position
-			Point currentPoint = Point(xCoord, yCoord);
-			int neighbourValueSum = NeighbourValueSum(currentPoint);
-
-			if (neighbourValueSum > _puzzleInput)
-			{
-				std::cout << "Day 2 Part 2 answer is: " << neighbourValueSum << std::endl;
-				break;
-			}
-
-			// Set the current point's value so it can be used to calculate the next point's value
-			// If the currentPoint is the starting point, set it to 1. Important to do this within the loop to keep the traversion intact.
-			_positionNumbers[currentPoint] = (currentPoint == Point(0, 0)) ? 1 : neighbourValueSum;
-
-			// Traverse the spiral
-			// Calculate the next numbers position
-			(horizontal) ? xCoord += direction : yCoord += direction;
-
-			// Increment step progression
-			++stepProgression;
-
-			// If this step has now been completed,
-			if (stepProgression == step)
-			{
-				// Flip the direction
-				horizontal = !horizontal;
-
-				// Reset the step progression
-				stepProgression = 0;
-
-				// When switched back from vertical to horizontal:
-				// * the step should be incremented
-				// * the direction changes (Horizontal: left <-> right. Vertical: up <-> down)
-				if (horizontal)
-				{
-					++step;
-					direction *= -1;
-				}
-			}
-		}
-	}
-
-#pragma endregion
-
-#pragma region Part1
-
-	void Part1()
-	{
-		GenerateSpiralMemory();
-		std::cout << "Day 2 Part 1 answer is:" << ManhattanDistance(Point(0, 0), _numberPositions[_puzzleInput]) << std::endl;
-	}
-#pragma endregion
-
-#pragma region Part2
-	void Part2()
-	{
-		GenerateSpiralMemory2();
-	}
-#pragma endregion
-}
+public:
+	void Part1();
+	void Part2();
+};

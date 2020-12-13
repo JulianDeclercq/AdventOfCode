33a5c8e9ec30cd65c0a2ee9245b9a05e7d0a1fc0
Julian <julian.declercq1@gmail.com>
2020-12-13 23:43:18 +0100

    * Day 13 Part 2

--- AdventOfCode2020/Day13.cpp
+++ AdventOfCode2020/Day13.cpp
@@ -2,8 +2,8 @@
 
 void Day13::ParseInput()
 {
-	//ifstream input("input/day13example.txt");
-	ifstream input("input/day13.txt");
+	ifstream input("input/day13example6.txt");
+	//ifstream input("input/day13.txt");
 
 	if (!input)
 	{
@@ -17,20 +17,31 @@ void Day13::ParseInput()
 
 	getline(input, line);
 	
+	int ctr = 0;
 	stringstream ss(line);
 	while (ss.good()) 
 	{
 		string substr;
 		getline(ss, substr, ',');
-		if(substr[0] != 'x')
-			_busses.push_back(stoi(substr));
+
+		if (substr[0] != 'x')
+		{
+			int bus = stoi(substr);
+			_busses.push_back(bus);
+			_offsets[bus] = ctr;
+		}
+		++ctr;
 	}
+	sort(_busses.begin(), _busses.end(), [](const int a, const int b) {return a > b; });
 }
 
 int Day13::PartOne()
 {
 	for (int bus : _busses)
 	{
+		if (bus == 0)
+			continue;
+
 		int earliestAvailable = (_target / bus + 1) * bus;
 		if (earliestAvailable < _earliest)
 		{
@@ -41,7 +52,27 @@ int Day13::PartOne()
 	return _busId * (_earliest - _target);
 }
 
-int Day13::PartTwo()
+long long Day13::PartTwo()
 {
+	// IDEA: it has to be divisable, so modulo 0
+	// idea is to check all the ones from the HIGHEST busnumbers (since this one is the LEAST frequent, and thus most performant to check (i suppose)) 
+	// then check if the second least frequent is offset correctly and so on until a solution has been found
+	//long long current = 100000000000000;
+	long long t = 0;
+	t -= t % _busses[0] +_offsets[_busses[0]];
+	while (true)
+	{
+		for (size_t i = 1; i < _busses.size(); ++i)
+		{
+			int bus = _busses[i];
+			long long timestampWatch = t + _offsets[bus];
+			if (timestampWatch % bus != 0)
+				break;
+
+			if (i == _busses.size() - 1)
+				return t;
+		}
+		t += _busses[0]; // next iteration
+	}
 	return 0;
 }
--- AdventOfCode2020/Day13.h
+++ AdventOfCode2020/Day13.h
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <sstream>
 #include <limits>
+#include <map>
 
 using namespace std;
 
@@ -15,8 +16,9 @@ class Day13
 private:
 	int _target = 0, _earliest = INT_MAX, _busId = 0;
 	vector<int> _busses;
+	map<int, int> _offsets;
 public:
 	void ParseInput();
 	int PartOne();
-	int PartTwo();
+	long long PartTwo();
 };

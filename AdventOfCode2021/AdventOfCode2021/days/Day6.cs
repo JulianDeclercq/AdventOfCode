be199ea187f4324f90d0fdb641134c74576023f8
Julian Declercq <julian.declercq1@gmail.com>
2021-12-07 02:04:53 +0100

    day 6 part 2

--- AdventOfCode2021/AdventOfCode2021/days/Day6.cs
+++ AdventOfCode2021/AdventOfCode2021/days/Day6.cs
@@ -1,42 +1,62 @@
-﻿namespace AdventOfCode2021.days;
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2021.days;
 
 public class Day6
 {
-    private static int _ctr = 0;
-    private Dictionary<int, int> _memo = new();
+    private readonly Dictionary<int, ulong> _memo = new();
     
-    private int ChildrenMadeNew(int startingValue, int daysLeft)
+    private ulong ChildrenMadeNew(int startingValue, int daysLeft)
     {
-        var children = 0;
+        // try to fetch from memo
+        if (startingValue == 8 && _memo.TryGetValue(daysLeft, out var m))
+            return m;
+        
+        ulong children = 0;
 
         // it takes startingValue + 1 days to make the first one
         if (daysLeft < startingValue + 1)
             return children;
         
         children++;
-        var newDaysLeft = daysLeft - (startingValue + 1);
+        var newDaysLeft = (daysLeft - (startingValue + 1));
         
         // for every 6 days left, a new one is created (that has value 8)
         var childrenMade = newDaysLeft / 7;
-        children += childrenMade;
+        children += (ulong) childrenMade;
 
         for (var i = 0; i < childrenMade; ++i)
             children += ChildrenMadeNew(8, newDaysLeft - i * 7);
 
+        // memo if starting value was 8
+        if (startingValue == 8)
+            _memo.Add(daysLeft, children);
+        
         return children;
+    }
+
+    private ulong Solve(int numberOfDays)
+    {
+        //var input = File.ReadLines(@"..\..\..\input\day6_example.txt").First().Split(',').Select(int.Parse).ToArray();
+        var input = File.ReadLines(@"..\..\..\input\day6.txt").First().Split(',').Select(int.Parse).ToArray();
+        
+        ulong ctr = 0;
+        foreach (var number in input)
+        {
+            var child = ChildrenMadeNew(number, numberOfDays);
+            ctr += child;
+        }
+        return ctr + (ulong) input.Length;
     }
   
     public void Part1()
     {
-        //var input = File.ReadLines(@"..\..\..\input\day6_example.txt").First().Split(',').Select(int.Parse).ToArray();
-        var input = File.ReadLines(@"..\..\..\input\day6.txt").First().Split(',').Select(int.Parse).ToArray();
-        var days = 80;
-        
-        foreach (var number in input)
-        {
-            var child = ChildrenMadeNew(number, days);
-            _ctr += child;
-        }
-        Console.WriteLine($"Day 6 part 1: {_ctr + input.Length}");
+        Console.WriteLine($"Day 6 part 1: {Solve(80)}");
+    }
+    
+    public void Part2()
+    {
+        Console.WriteLine($"Day 6 part 2: {Solve(256)}");
     }
 }

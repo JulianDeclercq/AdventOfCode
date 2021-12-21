9fa402b4210834c1fa326fa6d4cf533d294e7f8c
Julian Declercq <julian@geoguessr.com>
2021-12-21 15:58:51 +0100

    day 10 part 1

---
+++ AdventOfCode2021/AdventOfCode2021/days/Day10.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace AdventOfCode2021.days;
+
+public class Day10
+{
+    private static readonly Dictionary<char, char> _pairs = new()
+    {
+        {')', '('}, {']', '['}, {'}', '{'}, {'>', '<'},
+    };
+
+    private static readonly Dictionary<char, int> _score = new()
+    {
+        {')', 3}, {']', 57}, {'}', 1197}, {'>', 25137},
+    };
+    
+    public void Part1()
+    {
+        var lines = File.ReadAllLines(@"..\..\..\input\day10.txt");
+
+        var score = 0;
+        foreach(var line in lines)
+            score += CorruptionScore(line);
+        
+        Console.WriteLine($"Day 10 part 1: {score}");
+    }
+
+    private static int CorruptionScore(string line)
+    {
+        var stack = new Stack<char>();
+        foreach (var c in line)
+        {
+            // if the pairs contains it, it is a closing bracket
+            if (_pairs.TryGetValue(c, out var matching))
+            {
+                var peek = stack.Peek(); 
+                if (peek != matching)
+                    return _score[c];
+
+                stack.Pop();
+                continue;
+            }
+
+            // push the opening brackets to the stack
+            stack.Push(c);
+        }
+        return 0;
+    }
+}

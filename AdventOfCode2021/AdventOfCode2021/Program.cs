e001d2622c093f3c71caafa1c7669215db31bf97
Julian Declercq <julian@geoguessr.com>
2021-12-22 00:25:18 +0100

    day 12 refactor

--- AdventOfCode2021/AdventOfCode2021/Program.cs
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AdventOfCode2021.days;
 
 var day = new Day12();
-//day.Part1();
+day.Part1();
 day.Part2();
--- AdventOfCode2021/AdventOfCode2021/days/Day12.cs
+++ AdventOfCode2021/AdventOfCode2021/days/Day12.cs
@@ -18,9 +18,11 @@ public class Day12
     }
 
     private Dictionary<string, Cave> _caveSystem = new();
-    private List<List<string>> _allPaths = new();
     
-    public void Part1()
+    public void Part1() => Solve(part1: true);
+    public void Part2() => Solve(part1: false);
+
+    private void Solve(bool part1)
     {
         var lines = File.ReadAllLines(@"..\..\..\input\day12.txt");
 
@@ -47,61 +49,26 @@ public class Day12
             rhsCave.Connections.Add(lhsCave.Name);
         }
         
-        // find distinct paths
+        // find all paths
+        var allPaths = new List<List<string>>();
         foreach (var cave in _caveSystem["start"].Connections)
         {
             var path = new List<string>(){"start"};
-            NextStep(path, _caveSystem[cave]);
-        }
-        
-        Console.WriteLine($"Day 12 part 1: {_allPaths.Count}");
-    }
-    
-    public void Part2()
-    {
-        var lines = File.ReadAllLines(@"..\..\..\input\day12.txt");
 
-        // key = name
-        _caveSystem = new Dictionary<string, Cave>();
-        
-        // create the caves based on the names
-        foreach (var line in lines)
-        {
-            var split = line.Split('-');
-            _caveSystem[split[0]] = new Cave(split[0]);
-            _caveSystem[split[1]] = new Cave(split[1]);
+            if (part1) NextStep(allPaths, path, _caveSystem[cave]);
+            else NextStep2(allPaths, path, _caveSystem[cave], true);
         }
-        
-        // create the connections
-        foreach (var connection in lines)
-        {
-            var split = connection.Split('-');
-            
-            var lhsCave = _caveSystem[split[0]];
-            var rhsCave = _caveSystem[split[1]];
-            
-            lhsCave.Connections.Add(rhsCave.Name);
-            rhsCave.Connections.Add(lhsCave.Name);
-        }
-        
-        // find distinct paths
-        foreach (var cave in _caveSystem["start"].Connections)
-        {
-            var path = new List<string>(){"start"};
-            NextStep2(path, _caveSystem[cave], true);
-        }
-        
-        Console.WriteLine($"Day 12 part 2: {_allPaths.Count}");
+        Console.WriteLine($"Day 12 part {(part1 ? '1' : '2')}: {allPaths.Count}");
     }
 
-    private void NextStep(List<string> currentPath, Cave cave)
+    private void NextStep(List<List<string>> allPaths, List<string> currentPath, Cave cave)
     {
         var path = currentPath.ToList();
         path.Add(cave.Name);
         
         if (cave.Name.Equals("end"))
         {
-            _allPaths.Add(path);
+            allPaths.Add(path);
             return;
         }
         
@@ -111,18 +78,18 @@ public class Day12
             if (!c.Revisitable && path.Contains(connection))
                 continue;
             
-            NextStep(path, c);
+            NextStep(allPaths, path, c);
         }
     }
     
-    private void NextStep2(List<string> currentPath, Cave cave, bool canRevisitSmall)
+    private void NextStep2(List<List<string>> allPaths, List<string> currentPath, Cave cave, bool canRevisitSmall)
     {
         var path = currentPath.ToList();
         path.Add(cave.Name);
         
         if (cave.Name.Equals("end"))
         {
-            _allPaths.Add(path);
+            allPaths.Add(path);
             return;
         }
         
@@ -131,15 +98,14 @@ public class Day12
             var c = _caveSystem[connection];
             if (!c.Revisitable && path.Contains(connection))
             {
-                if(!canRevisitSmall || connection.Equals("start") || connection.Equals("end"))
-                    continue;
-                
-                // make an exception for this small cave
-                NextStep2(path, c, false);
+                // make an exception for this small cave if applicable
+                if (canRevisitSmall && !connection.Equals("start") && !connection.Equals("end"))
+                    NextStep2(allPaths, path, c, false);
+
                 continue;
             }
             
-            NextStep2(path, c, canRevisitSmall);
+            NextStep2(allPaths, path, c, canRevisitSmall);
         }
     }
 }

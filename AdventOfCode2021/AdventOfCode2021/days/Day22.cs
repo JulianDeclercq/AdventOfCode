76d0e0c29fdea1ae5561e4a48ee51b17fead4a93
Julian Declercq <julian@geoguessr.com>
2021-12-22 17:42:19 +0100

    nuked negative grids (it was a good exercise at least!) and went with simpler approach since neither of them works for part 2 anyways :)

--- AdventOfCode2021/AdventOfCode2021/days/Day22.cs
+++ AdventOfCode2021/AdventOfCode2021/days/Day22.cs
@@ -9,47 +9,40 @@ public class Day22
     
     public class Command
     {
-        public bool On { get; init; }
+        public bool TargetState { get; init; }
         public Point XRange { get; init; }
         public Point YRange { get; init; }
         public Point ZRange { get; init; }
         public bool ExceedsLimits = false;
 
         public override string ToString() =>
-            $"On: {On}, XRange: {XRange}, YRange {YRange}, ZRange {ZRange}, ExceedsLimits: {ExceedsLimits}";
+            $"On: {TargetState}, XRange: {XRange}, YRange {YRange}, ZRange {ZRange}, ExceedsLimits: {ExceedsLimits}";
     }
-    
+
+    private Dictionary<(int x, int y, int z), bool> CubeStates = new();
+
     public void Part1()
     {
         var lines = File.ReadAllLines(@"..\..\..\input\day22.txt");
         var commands = lines.Select(ParseCommand).Where(c => !c.ExceedsLimits);
 
-
-        var dim = 101; // -50 to 50 + 0 itself
-        // TODO: calculate width and height based on the origin and endpoint (endpoint is the opposite of origin, the absolute max x, y point)
-        var grid3D = new NegativeGrid3D<char>(dim, dim, dim, -50, -50, -50, '.');
         foreach (var command in commands)
-        {
-            ExecuteCommand(grid3D, command);
-        }
+            ExecuteCommand(command);
         
-        Console.WriteLine($"Day 22 part 1: {grid3D.All().Count(c => c == '#')}");
+        Console.WriteLine($"Day 22 part 1: {CubeStates.Count(c => c.Value)}");
     }
 
     private static Command ParseCommand(string line)
     {
         var groups = CommandPattern.Match(line).Groups;
 
-        var xMin = int.Parse(groups[2].Value);
-        var xMax = int.Parse(groups[3].Value);
-        var yMin = int.Parse(groups[4].Value);
-        var yMax = int.Parse(groups[5].Value);
-        var zMin = int.Parse(groups[6].Value);
-        var zMax = int.Parse(groups[7].Value);
+        int xMin = int.Parse(groups[2].Value), xMax = int.Parse(groups[3].Value);
+        int yMin = int.Parse(groups[4].Value), yMax = int.Parse(groups[5].Value);
+        int zMin = int.Parse(groups[6].Value), zMax = int.Parse(groups[7].Value);
         
         return new Command()
         {
-            On = groups[1].ToString().Equals("on"),
+            TargetState = groups[1].ToString().Equals("on"),
             XRange = new Point(xMin, xMax),
             YRange = new Point(yMin, yMax),
             ZRange = new Point(zMin, zMax),
@@ -58,7 +51,7 @@ public class Day22
         };
     }
 
-    private static void ExecuteCommand(NegativeGrid3D<char> grid, Command command)
+    private void ExecuteCommand(Command command)
     {
         for (var x = command.XRange.X; x <= command.XRange.Y; ++x)
         {
@@ -66,8 +59,7 @@ public class Day22
             {
                 for (var z = command.ZRange.X; z <= command.ZRange.Y; ++z)
                 {
-                    grid.Set(x, y, z, command.On ? '#' : '.');
-                    //Console.WriteLine($"Setting ({x}, {y}, {z}) to {command.On}");
+                    CubeStates[(x, y, z)] = command.TargetState; 
                 }
             }
         }

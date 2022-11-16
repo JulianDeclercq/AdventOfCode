5b9abb445b96c1ae3fe29b79dd116bee56b61713
Julian Declercq <julian.declercq1@gmail.com>
2022-11-16 23:48:38 +0100

    day 7 quick refactor

--- AdventOfCode2021/AdventOfCode2021/days/Day7.cs
+++ AdventOfCode2021/AdventOfCode2021/days/Day7.cs
@@ -2,48 +2,20 @@
 
 public class Day7
 {
-    private static int TotalFuel(IEnumerable<int> crabs, int targetPosition) 
-        => crabs.Sum(crab => Math.Abs(targetPosition - crab));
+    private static int TotalFuel(IEnumerable<int> crabs, int targetPosition)
+    {
+        return crabs.Sum(crab => Math.Abs(targetPosition - crab));
+    }
 
     private static int TotalFuel2(IEnumerable<int> crabs, int targetPosition)
     {
-        var answer = 0;
-        foreach (var crab in crabs)
-        {
-            // calculate the distance to the target position
-            var n = Math.Abs(targetPosition - crab);
-            
-            // calculate the fuel and add it to the answer
-            answer += (n * (n - 1) / 2) + n;
-        }
-        return answer;
+        return crabs.Select(crab => Math.Abs(targetPosition - crab)).Select(n => (n * (n - 1) / 2) + n).Sum();
     }
 
     private static int Solve(Func<IEnumerable<int>, int, int> totalFuelCalculator)
     {
         var crabs = File.ReadLines(@"..\..\..\input\day7.txt").First().Split(',').Select(int.Parse).ToArray();
-        
-        // determine the range of the loop (answer has to be between the two extremes of the crab positions)
-        var min = int.MaxValue;
-        var max = int.MinValue;
-        foreach (var crab in crabs)
-        {
-            if (crab < min)
-                min = crab;
-            
-            if (crab > max)
-                max = crab;
-        }
-
-        // find the position that requires least total fuel
-        var answer = int.MaxValue;
-        for (var i = min; i < max; ++i)
-        {
-            var fuel = totalFuelCalculator(crabs, i);
-            if (fuel < answer)
-                answer = fuel;
-        }
-        return answer;
+        return Enumerable.Range(crabs.Min(), crabs.Max()).Select(x => totalFuelCalculator(crabs, x)).Min();
     }
     
     public static void Part1() => Console.WriteLine($"Day 7 part 1: {Solve(TotalFuel)}");

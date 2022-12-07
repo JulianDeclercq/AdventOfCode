af68e26061ad18228538e1fa07bb16f9f045698e
Julian Declercq <julian.declercq1@gmail.com>
2022-12-07 23:30:12 +0100

    day 7 refactor

--- AdventOfCode2022/Days/Day7.cs
+++ AdventOfCode2022/Days/Day7.cs
@@ -17,10 +17,6 @@ public class Day7
 
     private class Item
     {
-        public Item()
-        {
-        }
-        
         public string Name = "";
         public ItemType Type = ItemType.None;
         public int Size = 0;
@@ -30,26 +26,7 @@ public class Day7
     private readonly Dictionary<string, List<Item>> _fileSystem = new();
     private readonly Dictionary<string, int> _directorySizes = new();
     
-    private int CalculateDirectorySize(string directoryName)
-    {
-        if (_directorySizes.TryGetValue(directoryName, out var foundSize))
-            return foundSize;
-
-        var size = CalculateSize(_fileSystem[directoryName]);
-        _directorySizes.Add(directoryName, size);
-        return size;
-    }
-
-    private int CalculateSize(IEnumerable<Item> path) // path = directory?
-    {
-        return path.Sum(item => item.Type switch
-        {
-            ItemType.Item => item.Size,
-            ItemType.Directory => CalculateDirectorySize(item.Name),
-        });
-    }
-
-    public void Solve(bool part1 = true)
+     public void Solve(bool part1 = true)
     {
         var lines = File.ReadAllLines(@"..\..\..\input\day7.txt");
 
@@ -109,25 +86,43 @@ public class Day7
             }
             else currentDir.Add(item);
         }
-
-        if (part1)
-        {
-            var result = _fileSystem.Select(path => CalculateSize(path.Value)).Where(size => size <= 100_000).Sum();
-            Console.WriteLine($"Day 7 part 1: {result}");
-            return;
-        }
-
-        const int totalSpace = 70_000_000;
-        const int requiredSpace = 30_000_000;
-        var usedSpace = CalculateDirectorySize("");
-        var unusedSpace = totalSpace - usedSpace;
-        var minimumDeletionSize = requiredSpace - unusedSpace;
-        // Console.WriteLine($"used space: {usedSpace}");
-        // Console.WriteLine($"unused space: {unusedSpace}");
-        // Console.WriteLine($"minimumDeletionSize: {minimumDeletionSize}");
-
-        var result2 = _fileSystem.Select(path => CalculateSize(path.Value)).Where(size => size > minimumDeletionSize)
-            .OrderBy(size => size).First();
-        Console.WriteLine($"Day 7 part 2: {result2}");
+        Console.WriteLine($"Day 7 part {(part1 ? 1 : 2)}: {(part1 ? Part1() : Part2())}");
     }
+
+     private int Part1()
+     {
+         return _fileSystem.Select(path => CalculateSize(path.Value)).Where(size => size <= 100_000).Sum();
+     }
+
+     private int Part2()
+     {
+         const int totalSpace = 70_000_000, requiredSpace = 30_000_000;
+         var usedSpace = CalculateDirectorySize("");
+         var unusedSpace = totalSpace - usedSpace;
+         var minimumDeletionSize = requiredSpace - unusedSpace;
+         
+         return _fileSystem.Select(path => CalculateSize(path.Value)).Where(size => size > minimumDeletionSize)
+             .OrderBy(size => size).First();
+     }
+    
+    private int CalculateDirectorySize(string directoryName)
+    {
+        if (_directorySizes.TryGetValue(directoryName, out var foundSize))
+            return foundSize;
+
+        var size = CalculateSize(_fileSystem[directoryName]);
+        _directorySizes.Add(directoryName, size);
+        return size;
+    }
+
+    private int CalculateSize(IEnumerable<Item> path) // path = directory?
+    {
+        return path.Sum(item => item.Type switch
+        {
+            ItemType.Item => item.Size,
+            ItemType.Directory => CalculateDirectorySize(item.Name),
+        });
+    }
+
+   
 }

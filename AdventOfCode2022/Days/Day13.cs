d075840ad89639e23ee15abd84ac1eeacc8b5a64
Julian Declercq <julian@geoguessr.com>
2022-12-13 17:32:23 +0100

    day 16 input

---
+++ AdventOfCode2022/Days/Day13.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2022.Days;
+
+public class Day13
+{
+    private class ListElement
+    {
+        public int Value = 0;
+        public List<ListElement> Contents = new();
+        public ListElementType Type = ListElementType.None;
+    }
+
+    private enum ListElementType
+    {
+        None = 0,
+        Integer = 1,
+        List = 2
+    }
+    
+    public void Solve()
+    {
+        var pairs = File.ReadAllLines(@"..\..\..\input\day13_example.txt").Where(l => !string.IsNullOrEmpty(l)).Chunk(2);
+        foreach (var pair in pairs)
+        {
+            var left = pair.First();
+            var right = pair.Last();
+            Console.WriteLine($"{left} | {right}");
+        }
+        
+        // list element could be an integer or another list
+
+    }
+
+    public void ParseList(string list)
+    {
+        var kek = new Stack<char>();
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,3 @@
 ﻿using AdventOfCode2022.Days;
 
-new Day11().Solve();
-new Day11().Solve(part1: false);
+new Day13().Solve();

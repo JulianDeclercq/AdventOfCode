2d2f13fb23d1e841f7690d896194dd1da2c4334f
Julian Declercq <julian@geoguessr.com>
2022-12-22 15:53:10 +0100

    day 21 part 2 fixed multiple correct answers because of integer division, fixed infinite loops in invalid checking

--- AdventOfCode2022/Days/Day21.cs
+++ AdventOfCode2022/Days/Day21.cs
@@ -14,18 +14,19 @@ public class Day21
     
     private enum Status
     {
-        None = 0,
+        Invalid = 0,
         TooLow = 1,
         Equal = 2,
         TooHigh = 3
     }
 
-    readonly Dictionary<string, long> _numberByMonkey = new();
-    readonly Dictionary<string, MonkeyOperation> _operationByMonkey = new();
+    private readonly Dictionary<string, long> _numberByMonkey = new();
+    private readonly Dictionary<string, MonkeyOperation> _operationByMonkey = new();
     private const string MyMonkey = "humn";
 
     private void ParseInput()
     {
+        // var lines = File.ReadAllLines(@"..\..\..\input\day21_example.txt");
         var lines = File.ReadAllLines(@"..\..\..\input\day21.txt");
 
         foreach (var line in lines)
@@ -51,35 +52,73 @@ public class Day21
     
     public void Solve2()
     {
-        ParseInput();    
+        ParseInput();
 
-        long last = 0;
-        // var input = 5_000_000_000_000;
-        var input = 3_665_520_865_950;
+        long last = 0, input = 5_000_000_000_000;
+        //input = 3_665_520_865_950;
+        // input = 3_665_520_865_941;
+        // input = 3_665_520_865_940;
+
+        long invalidCtr = 0;
         
         var answer1 = 3_665_520_865_942; // EQUAL, but submit says it's too high
         var answer2 = 3_665_520_865_941; // EQUAL, but submit says it's also too high 
         var answer3 = 3_665_520_865_940; // correct answer
 
+        var prevention = new HashSet<long>();
+
+        Console.WriteLine($"Starting input: {input}");
+        
+        // to prevent going back up / down again to the last check that failed and getting stuck in an infinite loop
+        var lastStatus = Status.Invalid;
         for (;;)
         {
-            var status = Cycle(input);
-
-            var halfDif = Math.Max(1, Math.Abs(last - input) / 2);
-            last = input;
-            
-            switch (status)
+            if (prevention.Contains(input))
             {
-                case Status.TooLow:
-                    input += halfDif;
-                    break;
-                case Status.Equal:
-                    Console.WriteLine($"Day 21 part 2: {input}");
-                    return;
-                case Status.TooHigh:
-                    input -= halfDif;
-                    break;
+                Console.WriteLine($"Caught duplicate {input}, exiting..");
+                return;
             }
+            prevention.Add(input);
+
+            var status = Cycle(input);
+            var halfDif = Math.Max(1, Math.Abs(last - input) / 2);
+
+            if (status is Status.Invalid)
+            {
+                switch (lastStatus)
+                {
+                    case Status.Invalid:
+                    case Status.TooLow:
+                        input++;
+                        break;
+                    case Status.TooHigh:
+                        input--;
+                        break;
+                    default: throw new ArgumentOutOfRangeException();
+                }
+                ++invalidCtr;
+                continue;
+            }
+            last = input;
+
+            checked
+            {
+                switch (status)
+                {
+                    case Status.TooLow:
+                        input += halfDif;
+                        break;
+                    case Status.Equal:
+                        Console.WriteLine($"Day 21 part 2: {input}");
+                        return;
+                    case Status.TooHigh:
+                        input -= halfDif;
+                        break;
+                }
+            }
+
+            lastStatus = status;
+            Console.WriteLine($"Trying {input} next.");
         }
     }
 
@@ -88,24 +127,35 @@ public class Day21
         _numberByMonkey[MyMonkey] = input;
 
         var operation = _operationByMonkey["root"];
-        var lhs = MonkeyValue(operation.Lhs);
+        var lhs = MonkeyValue(operation.Lhs); // cmmh was negative??
         var rhs = MonkeyValue(operation.Rhs);
-        var dif = rhs - lhs;
-        // Console.WriteLine($"{lhs} | {rhs} | {dif}");
 
-        return dif switch
+        if (lhs == null || rhs == null)
+            return Status.Invalid;
+
+        long dif = 0;
+        checked
+        {
+            //dif = lhs.Value - rhs.Value; // WORKS FOR EXAMPLE BUT NOT FOR INPUT
+            dif = rhs.Value - lhs.Value; // WORKS FOR INPUT BUT NOT FOR EXAMPLE
+        }
+
+        var status = dif switch
         {
             < 0 => Status.TooLow,
             0 => Status.Equal,
             > 0 => Status.TooHigh
         };
+        
+        Console.WriteLine($"Input: {input}, status: {status}, lhs:{lhs}, rhs: {rhs}, dif: {dif}");
+        return status;
     }
 
-    private long MonkeyValue(string monkey)
+    private long? MonkeyValue(string monkey)
     {
         if (_numberByMonkey.TryGetValue(monkey, out var number))
             return number;
-
+        
         var operation = _operationByMonkey[monkey];
         var lhs = MonkeyValue(operation.Lhs);
         var rhs = MonkeyValue(operation.Rhs);
@@ -117,11 +167,27 @@ public class Day21
                 "+" => lhs + rhs,
                 "-" => lhs - rhs,
                 "*" => lhs * rhs,
-                "/" => lhs / rhs,
+                // "/" => lhs / rhs,
+                "/" => Division(lhs, rhs),
                 _ => throw new ArgumentOutOfRangeException()
             };
+
+            if (value < 0)
+            {
+                int brkpt = 5;
+            }
             
             return value;
         }
+    }
+
+    // Avoid pursuing paths that have implicit flooring because of integer division 
+    private static long? Division(long? lhs, long? rhs)
+    {
+        var answer = lhs / rhs;
+        if (answer * rhs == lhs)
+            return answer;
+        
+        return null;
     }
 }

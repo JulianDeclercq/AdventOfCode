1ca3962793b065c93e00b277112dff901c9fe177
Julian Declercq <julian.declercq1@gmail.com>
2023-11-26 23:02:30 +0100

    read session token from config file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use std::sync::Arc;
+use serde::{Deserialize, Serialize};
+use std::{fs, sync::Arc};
 
 use reqwest::{blocking::Client, cookie::Jar, Url};
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    session_token: String,
+}
+
 fn main() -> reqwest::Result<()> {
+    let file = fs::read_to_string("config.json").expect("failed to read config");
+    let config: Config = serde_json::from_str(&file).unwrap();
     let input_url = "https://adventofcode.com/2015/day/6/input";
 
-    let cookie = "session=SESSION_COOKIE";
+    let cookie = format!("session={}", config.session_token);
     let url = input_url.parse::<Url>().unwrap();
 
     let jar = Jar::default();
-    jar.add_cookie_str(cookie, &url);
+    jar.add_cookie_str(cookie.as_str(), &url);
     let shared_jar = Arc::new(jar);
 
     let client = Client::builder()
@@ -18,7 +26,7 @@ fn main() -> reqwest::Result<()> {
         .build()?;
 
     let input = client.get(input_url).send()?.text()?;
-    println!("input: {:?}", input);
 
+    println!("input: {:?}", input);
     Ok(())
 }

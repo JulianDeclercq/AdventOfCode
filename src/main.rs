619460064098ee370e26fa2494d0217d77e216fd
Julian Declercq <julian.declercq1@gmail.com>
2023-11-26 20:18:31 +0100

    input fetching by request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use std::io;
+use std::sync::Arc;
 
-fn main() {
-    println!("type something..");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input");
+use reqwest::{blocking::Client, cookie::Jar, Url};
 
-    println!("user typed {}", input);
+fn main() -> reqwest::Result<()> {
+    let input_url = "https://adventofcode.com/2015/day/6/input";
+
+    let cookie = "session=SESSION_COOKIE";
+    let url = input_url.parse::<Url>().unwrap();
+
+    let jar = Jar::default();
+    jar.add_cookie_str(cookie, &url);
+    let shared_jar = Arc::new(jar);
+
+    let client = Client::builder()
+        .cookie_store(true)
+        .cookie_provider(shared_jar)
+        .build()?;
+
+    let input = client.get(input_url).send()?.text()?;
+    println!("input: {:?}", input);
+
+    Ok(())
 }

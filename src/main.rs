a433d96c079c710fbe971bf5c7b836ea8e992e25
Julian Declercq <julian.declercq1@gmail.com>
2023-11-28 22:43:46 +0100

    split up input into a module

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,6 @@
-use serde::{Deserialize, Serialize};
-use std::{fs, sync::Arc};
-
-use reqwest::{blocking::Client, cookie::Jar, Url};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Config {
-    session_token: String,
-}
+mod input;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file = fs::read_to_string("config.json").expect("failed to read config");
-    let config: Config = serde_json::from_str(&file)?;
-    let input_url = "https://adventofcode.com/2015/day/6/input";
-
-    let cookie = format!("session={}", config.session_token);
-    let url = input_url.parse::<Url>()?;
-
-    let jar = Jar::default();
-    jar.add_cookie_str(cookie.as_str(), &url);
-    let shared_jar = Arc::new(jar);
-
-    let client = Client::builder()
-        .cookie_store(true)
-        .cookie_provider(shared_jar)
-        .build()?;
-
-    let input = client.get(input_url).send()?.text()?;
-
-    println!("input: {}", input);
+    input::read_input_for_year(6, 2015)?;
     Ok(())
 }

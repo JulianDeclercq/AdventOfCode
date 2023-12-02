c5907e259368277e64e0983c877701ab19ce8a57
Julian Declercq <julian.declercq1@gmail.com>
2023-12-02 15:27:52 +0100

    added C# solutions for day 1 and 2

---
+++ AdventOfCode2023/days/Day1.cs
@@ -0,0 +1,64 @@
+﻿namespace AdventOfCode2023;
+
+public class Day1
+{
+    private readonly Dictionary<string, int> _lookup = new()
+    {
+        ["one"] = 1, ["two"] = 2, ["three"] = 3, ["four"] = 4, ["five"] = 5,
+        ["six"] = 6, ["seven"] = 7, ["eight"] = 8, ["nine"]= 9
+    };
+
+    private readonly HashSet<string> _numberStrings = new()
+    {
+        "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    };
+
+    private readonly string[] _lines = File.ReadAllLines("../../../input/Day1.txt");
+
+    public void Part1() => Console.WriteLine(_lines.Sum(l => int.Parse($"{l.First(char.IsNumber)}{l.Last(char.IsNumber)}")));
+    public void Part2() => Console.WriteLine(_lines.Sum(GetLineNumber));
+
+    private int GetLineNumber(string line)
+    {
+        string lowestNumber = "", highestNumber = "";
+        int lowestIdx = int.MaxValue, highestIdx = int.MinValue;
+        foreach (var ns in _numberStrings)
+        {
+            var firstIdx = line.IndexOf(ns, StringComparison.Ordinal);
+            if (firstIdx > -1)
+            {
+                if (firstIdx < lowestIdx)
+                {
+                    lowestNumber = ns;
+                    lowestIdx = firstIdx;
+                }    
+            }
+
+            var lastIdx = line.LastIndexOf(ns, StringComparison.Ordinal);
+            if (lastIdx > -1)
+            {
+                if (lastIdx > highestIdx)
+                {
+                    highestNumber = ns;
+                    highestIdx = lastIdx;
+                }
+            }
+        }
+        
+        var left = StringToNumber(lowestNumber);
+        var right = StringToNumber(highestNumber);
+        return int.Parse($"{(left > 0 ? left : "")}{(right > 0 ? right : "")}");
+    }
+
+    private int StringToNumber(string input)
+    {
+        if (input.Length == 0)
+            return 0;
+        
+        if (input.Length == 1)
+            return int.Parse(input);
+       
+        return _lookup[input];
+    }
+}
---
+++ AdventOfCode2023/days/Day2.cs
@@ -0,0 +1,92 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.days;
+
+public class Day2
+{
+    // Cut out the "Game X:" and add a trailing ";" to make parsing easier
+    private readonly string[] _lines =
+        File.ReadAllLines("../../../input/Day2.txt").Select(l => $"{l[(l.IndexOf(':') + 1)..]};").ToArray();
+    private readonly Regex _setPattern = new("([^;]+);");
+    private readonly Regex _cubePattern = new(@"(\d+) (\w+)");
+
+    public void Part1()
+    {
+        var games = ParseGames();
+        var loadedConfig = new CubeSet { Red = 12, Green = 13, Blue = 14 };
+
+        Console.WriteLine(games.Where(g => g.CubeSets.All(cs => cs.FitsConfig(loadedConfig))).Sum(g => g.Number));
+    }
+
+    public void Part2()
+    {
+        var games = ParseGames();
+        Console.WriteLine(games.Select(g => g.MinimumSet()).Sum(s => s.Power));
+    }
+
+    private IReadOnlyCollection<Game> ParseGames()
+    {
+        var games = new List<Game>();
+        for (var i = 0; i < _lines.Length; ++i)
+        {
+            var game = new Game(i + 1);
+            var sets = _setPattern.Matches(_lines[i]);
+            var cubesPerSet = sets.Select(match => _cubePattern.Matches(match.Groups[1].ToString()));
+
+            foreach (var cubeSet in cubesPerSet)
+            {
+                var set = new CubeSet();
+                foreach (Match cubeMatch in cubeSet)
+                {
+                    var amount = int.Parse(cubeMatch.Groups[1].ToString());
+                    switch (cubeMatch.Groups[2].ToString())
+                    {
+                        case "red": set.Red += amount; break;
+                        case "blue": set.Blue += amount; break;
+                        case "green": set.Green += amount; break;
+                    } 
+                }
+                game.CubeSets.Add(set);
+            }
+            games.Add(game);
+        }
+
+        return games;
+    }
+
+    private class Game
+    {
+        public Game(int number)
+        {
+            Number = number;
+        }
+        
+        public readonly int Number = 0;
+        public readonly List<CubeSet> CubeSets = new();
+
+        public CubeSet MinimumSet()
+        {
+            return new CubeSet
+            {
+                Red = CubeSets.Max(cs => cs.Red),
+                Green = CubeSets.Max(cs => cs.Green),
+                Blue = CubeSets.Max(cs => cs.Blue),
+            };
+        }
+    }
+
+    private class CubeSet
+    {
+        public int Red = 0;
+        public int Green = 0;
+        public int Blue = 0;
+
+        public int Power => Red * Green * Blue;
+        
+        public bool FitsConfig(CubeSet config)
+        {
+            return Red <= config.Red && Green <= config.Green && Blue <= config.Blue;
+        }
+    }
+}
+

b01f2c79a78897798b27b258742b4a2096a5a2e4
Julian Declercq <julian.declercq1@gmail.com>
2023-12-03 14:51:27 +0100

    2023 day 3 part 2 example

--- 2023/AdventOfCode2023/Program.cs
+++ 2023/AdventOfCode2023/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2023.days;
 
 var day = new Day3();
-day.Part1();
+day.Part2();
--- 2023/AdventOfCode2023/days/Day3.cs
+++ 2023/AdventOfCode2023/days/Day3.cs
@@ -51,6 +51,68 @@ public class Day3
         Console.WriteLine(sum);
     }
 
+    private static bool IsGear(GridElement<char> ge) => ge.Value.Equals('*'); 
+    
+    public void Part2()
+    {
+        //const string inputFileName = "../../../input/Day3_example.txt";
+        const string inputFileName = "../../../input/Day3.txt";
+        var lines = File.ReadAllLines(inputFileName);
+        var grid = new Grid<char>(lines.First().Length, lines.Length, lines.SelectMany(l => l), 'A');
+
+        var neighbours = new List<GridElement<char>>();
+        var gears = new Dictionary<Point, List<int>>();
+        for (var i = 0; i < grid.Height; ++i)
+        {
+            var row = grid.Row(i).ToArray();
+            var current = "";
+            for (var j = 0; j < row.Length; ++j)
+            {
+                var cell = row[j];
+                
+                if (char.IsDigit(cell.Value))
+                {
+                    current += cell.Value;
+                    neighbours.AddRange(grid.NeighbouringPointsExtended(cell.Position));
+                    
+                    if (j == row.Length - 1) // handle last
+                    {
+                        var neighbouringGears = neighbours.Where(IsGear).ToArray();
+                        foreach (var gear in neighbouringGears)
+                        {
+                            var numbers = gears.TryGetValue(gear.Position, out var n) ? n : new List<int>();
+                            numbers.Add(int.Parse(current));
+                            gears[gear.Position] = numbers;
+                        }
+                        neighbours.Clear();
+                        current = "";
+                    }
+                    continue;
+                }
+
+                if (!string.IsNullOrWhiteSpace(current))
+                {
+                    var neighbouringGears = neighbours.Where(IsGear).ToArray();
+                    foreach (var gear in neighbouringGears)
+                    {
+                        var numbers = gears.TryGetValue(gear.Position, out var n) ? n : new List<int>();
+                        numbers.Add(int.Parse(current));
+                        gears[gear.Position] = numbers;
+                    }
+                    neighbours.Clear();
+                    current = "";
+                }
+            }
+        }
+
+        // TODO: MAYBE IN THE INPUT DISTINCT IS TOO NAIVE, IF THE GEAR IS ON THE BORDER OF 2 IDENTICAL VALUE, BUT DIFFERENT NUMBERS
+        var answer = gears
+            .Select(g => g.Value.Distinct().ToArray())
+            .Where(g => g.Length == 2)
+            .Sum(g => g.Aggregate(1, (curr, next) => curr * next));
+        Console.WriteLine(answer);
+    }
+
     private static readonly HashSet<char> Symbols = new() { '/', '*', '%', '$', '@', '&', '=', '+', '#', '-'};
     private static bool IsSymbol(char c) => Symbols.Contains(c);
 }

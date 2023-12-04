0166738ff7851a26371b9af3ba54ae0528ff0dfb
Julian Declercq <julian.declercq1@gmail.com>
2023-12-05 00:25:50 +0100

    2019 day 4 refactor

--- 2019/C#/AdventOfCode2019/days/Day4.cs
+++ 2019/C#/AdventOfCode2019/days/Day4.cs
@@ -2,45 +2,23 @@
 
 namespace AdventOfCode2019.days;
 
-public class Day4
+public partial class Day4
 {
-    private const string input = "165432-707912";
-    public void Part1()
+    public static void Solve()
     {
-        //Console.WriteLine(Enumerable.Range(165432, 707912 - 165431).Count(Valid));
-
-        var test = Valid3(112233);
-        var test2 = Valid3(123444);
-        var test3 = Valid3(111122);
-
-        //Console.WriteLine(Enumerable.Range(165432, 707912 - 165431).Count(Valid2));
-        int ctr = 0;
-        foreach (var haha in Enumerable.Range(165432, 707912 - 165431))
-        {
-            if (Valid3(haha))
-            {
-                //Console.WriteLine($"valid: {haha}");
-                Console.WriteLine(haha);
-                ctr++;
-            }
-        }
-        Console.WriteLine(ctr);
+        var range = Enumerable.Range(165432, 707912 - 165431).ToArray();
+        Console.WriteLine(range.Count(nr => Validate(nr, part: 1)));
+        Console.WriteLine(range.Count(nr => Validate(nr, part: 2)));
     }
 
-    private bool Valid(int nr)
+    private static bool Validate(int nr, int part)
     {
         var str = nr.ToString();
-        var hasAdjacent = false;
-        for (var i = 0; i < str.Length - 1; ++i)
-        {
-            if (str[i] == str[i + 1])
-            {
-                hasAdjacent = true;
-                break;
-            }
-        }
 
-        if (!hasAdjacent)
+        var adjacentParts = RepeatedAdjacent().Matches(str);
+        var validAdjacent = part == 1 ? adjacentParts.Any() : adjacentParts.Any(ep => ep.Length == 2);
+
+        if (!validAdjacent)
             return false;
 
         var numbers = str.Select(x => int.Parse($"{x}")).ToArray();
@@ -48,53 +26,6 @@ public class Day4
         return numbers.SequenceEqual(ordered);
     }
     
-    private bool Valid2(int nr)
-    {
-        var str = nr.ToString();
-
-        var equalParts = new List<string>();
-        var builder = $"{str[0]}";
-        var previous = str[0];
-        for (var i = 1; i < str.Length; ++i)
-        {
-            var current = str[i];
-            if (i == str.Length - 1) // handle last
-            {
-                if (previous == current)
-                {
-                    builder += current;
-                    equalParts.Add(builder);    
-                }
-                break;
-            }
-            
-            if (current != previous)
-            {
-                equalParts.Add(builder);
-                builder = "";
-            }
-            builder += current;
-            previous = current;
-        }
-        
-        if (equalParts.All(ep => ep.Length != 2))
-            return false;
-
-        var numbers = str.Select(x => int.Parse($"{x}")).ToArray();
-        var ordered = numbers.Order().ToArray();
-        return numbers.SequenceEqual(ordered);
-    }
-    
-    private bool Valid3(int nr)
-    {
-        var str = nr.ToString();
-
-        var reg = new Regex("(.)\\1+");
-        if (reg.Matches(str).All(ep => ep.Length != 2))
-            return false;
-
-        var numbers = str.Select(x => int.Parse($"{x}")).ToArray();
-        var ordered = numbers.Order().ToArray();
-        return numbers.SequenceEqual(ordered);
-    }
+    [GeneratedRegex(@"(.)\1+")]
+    private static partial Regex RepeatedAdjacent();
 }

6e849c12382f0236e13220b58df14db07dec5bf3
Julian Declercq <julian@geoguessr.com>
2023-12-04 16:16:07 +0100

    quick draft

--- 2023/C#/AdventOfCode2023/Program.cs
+++ 2023/C#/AdventOfCode2023/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode2023.days;
 
-new Day4().Part1();
+new Day4().Part2();
--- 2023/C#/AdventOfCode2023/days/Day4.cs
+++ 2023/C#/AdventOfCode2023/days/Day4.cs
@@ -43,9 +43,55 @@ public partial class Day4
     
     public void Part2()
     {
-        
+        var pointValues = new List<int>();
+        var helper = new RegexHelper(InputRegex(), "winning", "candidates");
+
+        var lines = File.ReadAllLines("../../../input/Day4_example.txt");
+        for (var i = 0; i < lines.Length; ++i)
+        {
+            var line = lines[i];
+            if (!helper.Match(line))
+            {
+                Console.WriteLine($"Failed to match line {line}");
+                continue;
+            }
+
+            var winning = helper.Get("winning")
+                .Split(' ')
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(int.Parse)
+                .ToHashSet();
+
+            var candidates = helper.Get("candidates")
+                .Split(' ')
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(int.Parse)
+                .ToHashSet();
+
+            winning.IntersectWith(candidates);
+
+            if (winning.Count == 0)
+                continue;
+            
+            // TODO: Be careful for indices vs card nrs, and in general with off by 1 here lol.
+            var cardsLeft = lines.Length - 1 - i;
+            var winners = Math.Min(winning.Count, cardsLeft); // TODO: Check if this check is needed / makes sense
+            var idx = i;
+            RegisterCopies(Enumerable.Range(i + 2, winners).Select(x => x + idx));
+        }
+        Console.WriteLine(pointValues.Sum());
     }
 
+    private readonly Dictionary<int, int> _cardOccurrences = new(); // key: cardNr, value: occurrences
+    private void RegisterCopies(IEnumerable<int> cardNrs)
+    {
+        foreach (var cardNr in cardNrs)
+        {
+            var occurrences = _cardOccurrences.TryGetValue(cardNr, out var o) ? o : 0;
+            _cardOccurrences[cardNr] = occurrences + 1;
+        }
+    }
+    
     [GeneratedRegex(".+: (.+) \\| (.+)")]
     private static partial Regex InputRegex();
 }

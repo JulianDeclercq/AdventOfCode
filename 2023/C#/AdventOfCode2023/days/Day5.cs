1278457c1edd99951e9276b3e0afd0daad359118
Julian Declercq <julian.declercq1@gmail.com>
2023-12-05 22:33:06 +0100

    2023 day 5 part 2 refactor

--- 2023/C#/AdventOfCode2023/days/Day5.cs
+++ 2023/C#/AdventOfCode2023/days/Day5.cs
@@ -20,17 +20,7 @@ public class Day5
     
     private class Map
     {
-        public Map(List<SubMap> submaps)
-        {
-            SubMaps = submaps;
-        }
-
-        public readonly List<SubMap> SubMaps;
-    }
-      
-    private class SubMap
-    {
-        public SubMap(long destinationStart, long sourceStart, long length)
+        public Map(long destinationStart, long sourceStart, long length)
         {
             _destinationStart = destinationStart;
             _sourceStart = sourceStart;
@@ -57,49 +47,38 @@ public class Day5
             return _sourceStart + (destinationNr - _destinationStart);
         }
 
-        public bool InBounds(long sourceNr)
-        {
-            if (sourceNr < _sourceStart || sourceNr >= _sourceStart + _length)
-                return false;
-
-            return true;
-        }
-
-        public long MinimumSource => _sourceStart;
-        public long MinimumDestination => _destinationStart;
-        public long MaximumDestination => _destinationStart + _length - 1;
+        public bool InSourceBounds(long sourceNr) => _sourceStart <= sourceNr && sourceNr < _sourceStart + _length;
+        public bool InDestinationBounds(long destinationNr) => _destinationStart <= destinationNr && destinationNr < _destinationStart + _length;
     }
 
-    private static Map ParseMap(IEnumerable<string> lines, ref int offset)
+    private static List<Map> ParseMap(IEnumerable<string> lines, ref int offset)
     {
-        var subMaps = lines
+        var maps = lines
             .Skip(offset) // skip previous maps
             .TakeWhile(l => !string.IsNullOrWhiteSpace(l)) // go until next map
             .Select(l => l.Split(' '))
-            .Select(x => new SubMap(long.Parse(x[0]), long.Parse(x[1]), long.Parse(x[2])))
-            .OrderBy(x => x.MinimumSource) // this sort is safe because no ranges overlap in the input.
+            .Select(x => new Map(long.Parse(x[0]), long.Parse(x[1]), long.Parse(x[2])))
             .ToList();
 
-        offset += subMaps.Count + 2; // +2 to skip the empty line and the "X-to-X map:" line
-        return new Map(subMaps);
+        offset += maps.Count + 2; // +2 to skip the empty line and the "X-to-X map:" line
+        return maps;
     }
 
-    private Map? _seedToSoil, _soilToFertilizer, _fertilizerToWater, _waterToLight, _lightToTemperature,
+    private List<Map>? _seedToSoil, _soilToFertilizer, _fertilizerToWater, _waterToLight, _lightToTemperature,
         _temperatureToHumidity, _humidityToLocation;
     
     public void Solve(bool part1)
     {
         var offset = 0;
-        //var lines = File.ReadAllLines("../../../input/Day5_example.txt");
         var lines = File.ReadAllLines("../../../input/Day5.txt");
         var seedInput = lines.First()["seeds: ".Length..].Split(' ').Select(long.Parse).ToList();
         offset += 3;
 
-        var ranges = new List<Range>();
+        var seedRanges = new List<Range>();
         if (!part1) // part 2: treat the seeds as ranges
         {
             for (var i = 0; i < seedInput.Count - 1; i += 2)
-                ranges.Add(new Range(seedInput[i], seedInput[i + 1]));
+                seedRanges.Add(new Range(seedInput[i], seedInput[i + 1]));
         }
         
         _seedToSoil = ParseMap(lines, ref offset);
@@ -120,16 +99,15 @@ public class Day5
         for (long i = 0;; ++i)
         {
             if (i != 0 && i % 1_000_000 == 0)
-                Qonsole.OverWrite($"{i / 1_000_000} million iterations{string.Concat(Enumerable.Repeat('.', (int)(i / 1_000_000) % 3))}");
+                Qonsole.OverWrite($"{i / 1_000_000} million iterations{string.Concat(Enumerable.Repeat('.', (int)(i / 1_000_000) % 3 + 1))}");
 
             var seed = LocationToSeed(i);
-            if (ranges.Any(range => range.Contains(seed)))
+            if (seedRanges.Any(range => range.Contains(seed)))
             {
-                Qonsole.OverWrite($"answer is {i}");
+                Qonsole.OverWrite($"{i}");
                 break;
             }    
         }
-        
     }
     
     private long SeedToLocation(long seed)
@@ -142,6 +120,12 @@ public class Day5
         var humidity = GetDestination(_temperatureToHumidity!, temperature);
         var location = GetDestination(_humidityToLocation!, humidity);
         return location;
+    }
+    
+    private static long GetDestination(IEnumerable<Map> maps, long source)
+    {
+        var submap = maps.FirstOrDefault(sm => sm.InSourceBounds(source));
+        return submap?.MapToDestination(source) ?? source;
     }
 
     private long LocationToSeed(long location)
@@ -157,23 +141,9 @@ public class Day5
         return seed;
     }
 
-    private static long GetDestination(Map map, long source)
+    private static long GetSource(IEnumerable<Map> maps, long destination)
     {
-        var submap = map.SubMaps.FirstOrDefault(osm => osm.InBounds(source));
-        return submap?.MapToDestination(source) ?? source;
-    }
-    
-    private static long GetSource(Map map, long destination)
-    {
-        foreach (var el in map.SubMaps)
-        {
-            if (el.MinimumDestination <= destination && destination <= el.MaximumDestination)
-            {
-                return el.MapToSource(destination);
-            }
-        }
-        
-        // nothing found, return destination itself
-        return destination;
+        var submap = maps.FirstOrDefault(sm => sm.InDestinationBounds(destination));
+        return submap?.MapToSource(destination) ?? destination;
     }
 }
--- 2023/C#/AdventOfCode2023/helpers/Qonsole.cs
+++ 2023/C#/AdventOfCode2023/helpers/Qonsole.cs
@@ -4,8 +4,9 @@ public static class Qonsole
 {
     public static void OverWrite(string newMessage)
     {
-        var eraseOld = string.Concat(Enumerable.Repeat('\b', _lastMessage.Length));
-        Console.Write($"{eraseOld}{newMessage}");
+        var eraseOld = string.Concat(Enumerable.Repeat("\b \b", _lastMessage.Length));
+        Console.Write(eraseOld);
+        Console.Write(newMessage);
         _lastMessage = newMessage;
     }
 

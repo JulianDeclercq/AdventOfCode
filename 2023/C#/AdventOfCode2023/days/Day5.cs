186882a5f6de666e4cbb00d5677afbe9a11cf217
Julian Declercq <julian.declercq1@gmail.com>
2023-12-05 18:28:09 +0100

    refactoring

--- 2023/C#/AdventOfCode2023/days/Day5.cs
+++ 2023/C#/AdventOfCode2023/days/Day5.cs
@@ -1,41 +1,10 @@
-﻿namespace AdventOfCode2023.days;
+﻿using AdventOfCode2023.helpers;
+
+namespace AdventOfCode2023.days;
 
 public class Day5
 {
-    private class SubMap
-    {
-        public SubMap(long destinationStart, long sourceStart, long length)
-        {
-            _destinationStart = destinationStart;
-            _sourceStart = sourceStart;
-            _length = length;    
-        }
-        
-        private readonly long _destinationStart;
-        private readonly long _sourceStart;
-        private readonly long _length;
-
-        public (bool inRange, long result) MapToDestination(long sourceNr)
-        {
-            if (sourceNr < _sourceStart || sourceNr >= _sourceStart + _length)
-                return new(false, sourceNr);
-
-            return new(true, _destinationStart + (sourceNr - _sourceStart));
-        }
-
-        public bool InBounds(long sourceNr)
-        {
-            if (sourceNr < _sourceStart || sourceNr >= _sourceStart + _length)
-                return false;
-
-            return true;
-        }
-
-        public long MinimumSource => _sourceStart;
-        public long MaximumSource => _sourceStart + _length - 1;
-    }
-
-    private class Map
+      private class Map
     {
         public Map(List<SubMap> orderedSubmaps)
         {
@@ -47,9 +16,10 @@ public class Day5
 
         public bool InRange(long nr) => MinElement <= nr && nr <= MaxElement;
         public List<SubMap> OrderedSubMaps;
-
+        
         public SubMap? AdaptedBinarySearchCorrectMap(long value)
         {
+            return OrderedSubMaps.FirstOrDefault(osm => osm.InBounds(value));
             var left = 0;
             var right = OrderedSubMaps.Count - 1;
             while (left <= right)
@@ -72,6 +42,69 @@ public class Day5
             return null;
         }
     }
+      
+    private class SubMap
+    {
+        public SubMap(long destinationStart, long sourceStart, long length)
+        {
+            _destinationStart = destinationStart;
+            _sourceStart = sourceStart;
+            _length = length;    
+        }
+        
+        private readonly long _destinationStart;
+        private readonly long _sourceStart;
+        private readonly long _length;
+
+        public (bool inRange, long result) MapToDestination(long sourceNr)
+        {
+            if (sourceNr < _sourceStart || sourceNr >= _sourceStart + _length)
+                return new(false, sourceNr);
+
+            return new(true, _destinationStart + (sourceNr - _sourceStart));
+        }
+
+        public (bool inRange, long result) MapToSource(long destinationNr)
+        {
+            if (destinationNr < _destinationStart || destinationNr >= _destinationStart + _length)
+                return new(false, destinationNr);
+
+            return new(true, _sourceStart + (destinationNr - _destinationStart));
+        }
+
+        public bool InBounds(long sourceNr)
+        {
+            if (sourceNr < _sourceStart || sourceNr >= _sourceStart + _length)
+                return false;
+
+            return true;
+        }
+
+        public bool InBoundsDestination(long destinationNr)
+        {
+            if (destinationNr < _destinationStart || destinationNr >= _destinationStart + _length)
+                return false;
+
+            return true;
+        }
+
+        public long MinimumSource => _sourceStart;
+        public long MaximumSource => _sourceStart + _length - 1;
+        public long MinimumDestination => _destinationStart;
+        public long MaximumDestination => _destinationStart + _length - 1;
+       
+        public IEnumerable<long> PossibleSources()
+        {
+            for (var i = MinimumSource; i <= MaximumSource; ++i)
+                yield return i;
+        }
+        
+        public IEnumerable<long> PossibleDestinations()
+        {
+            for (var i = MinimumDestination; i <= MaximumDestination; ++i)
+                yield return i;
+        }
+    }
 
     private static Map ParseMap(IEnumerable<string> lines, ref int offset)
     {
@@ -81,33 +114,54 @@ public class Day5
             .Select(l => l.Split(' '))
             .Select(x => new SubMap(long.Parse(x[0]), long.Parse(x[1]), long.Parse(x[2])))
             .OrderBy(x => x.MinimumSource) // this sort is safe because no ranges overlap in the input.
+            //.OrderBy(x => x.MinimumDestination) // IDK IF THIS IS FINE, PROBALBY NOT
             .ToList();
 
         offset += subMaps.Count + 2; // +2 to skip the empty line and the "X-to-X map:" line
         return new Map(subMaps);
     }
 
-    private void AddSeeds(List<long> seeds, long start, long length)
-    {
-        for (long i = 0; i < length; ++i)
-            seeds.Add(start + i);
-    }
-
     private Map? _seedToSoil, _soilToFertilizer, _fertilizerToWater, _waterToLight, _lightToTemperature,
         _temperatureToHumidity, _humidityToLocation;
+
+    private class JulleRange
+    {
+        public JulleRange(long start, long length)
+        {
+            Min = start;
+            Max = start + length - 1;
+            Length = length;
+        }
+
+        public bool Contains(long value) => Min <= value && value <= Max;
+        
+        public long Min;
+        public long Max;
+        public long Length;
+    }
     public void Solve()
     {
+        const bool part1 = true;
+        var console = new ConsoleWriter();
+        
         var offset = 0;
         //var lines = File.ReadAllLines("../../../input/Day5_example.txt");
         var lines = File.ReadAllLines("../../../input/Day5.txt");
-        var seedRanges = lines.First()["seeds: ".Length..].Split(' ').Select(long.Parse).ToArray();
+        var seedInput = lines.First()["seeds: ".Length..].Split(' ').Select(long.Parse).ToList();
         offset += 3;
-        
-        var seeds = new List<long>(2_000_000_000);
-        if (seedRanges.Length % 2 != 0) throw new Exception("cant make ranges");
 
-        for (var i = 0; i < seedRanges.Length - 1; i += 2)
-            AddSeeds(seeds, seedRanges[i], seedRanges[i + 1]);
+        Console.WriteLine("Parsing seeds");
+        var parsedSeeds = new HashSet<long>();
+        var ranges = new List<JulleRange>();
+        if (part1) 
+        {
+            parsedSeeds = seedInput.ToHashSet();
+        }
+        else // part 2: treat the seeds as ranges
+        {
+            for (var i = 0; i < seedInput.Count - 1; i += 2)
+                ranges.Add(new JulleRange(seedInput[i], seedInput[i + 1]));
+        }
         
         _seedToSoil = ParseMap(lines, ref offset);
         _soilToFertilizer = ParseMap(lines, ref offset);
@@ -117,55 +171,110 @@ public class Day5
         _temperatureToHumidity = ParseMap(lines, ref offset);
         _humidityToLocation = ParseMap(lines, ref offset);
 
-        var answer = long.MaxValue;
-        foreach (var seed in seeds)
+        for (long i = 0;; ++i)
         {
-            if (_iterations % 1_000_000 == 0)
-                Console.WriteLine($"{_iterations}/{seeds.Count} {((float)_iterations/seeds.Count)*100}%");
+            if (i % 1_000_000 == 0)
+            {
+                if (i == 500_000_000)
+                {
+                    Console.WriteLine($"Failed to find solution after 500 million iterations");
+                    break;
+                }
+                console.OverWrite($"{i / 1_000_000} million iterations");
+                //Console.WriteLine($"{i / 1_000_000} million iterations");
+            }
 
-            var location = SeedToLocation(seed);
+            var seed = LocationToSeed(i);
 
-            if (location < answer)
-                answer = location;
+            if (part1)
+            {
+                if (parsedSeeds.Contains(seed))
+                {
+                    Console.WriteLine($"answer is {i}");
+                    break;
+                }    
+            }
+            else
+            {
+                if (ranges.Any(r => r.Contains(seed)))
+                {
+                    Console.WriteLine($"answer is {i}");
+                    break;
+                }    
+            }
         }
-        Console.WriteLine(answer);
+        
     }
 
-    private int _iterations = 0;
+    private long LocationToSeed(long location)
+    {
+        var humidity = GetSourceDebug(_humidityToLocation!, location, "_temperatureToHumidity");
+        var temperature = GetSourceDebug(_temperatureToHumidity!, humidity, "_temperatureToHumidity");
+        var light = GetSourceDebug(_lightToTemperature!, temperature, "_lightToTemperature");
+        var water = GetSourceDebug(_waterToLight!, light, "_waterToLight");
+        var fertilizer = GetSourceDebug(_fertilizerToWater!, water, "_fertilizerToWater");
+        var soil = GetSourceDebug(_soilToFertilizer!, fertilizer, "_soilToFertilizer");
+        var seed = GetSourceDebug(_seedToSoil!, soil, "_seedToSoil");
+
+        return seed;
+    }
     
     private long SeedToLocation(long seed)
     {
-        var soil = GetValue(_seedToSoil!, seed);
-        var fertilizer = GetValue(_soilToFertilizer!, soil);
-        var water = GetValue(_fertilizerToWater!, fertilizer);
-        var light = GetValue(_waterToLight!, water);
-        var temperature = GetValue(_lightToTemperature!, light);
-        var humidity = GetValue(_temperatureToHumidity!, temperature);
-        var location = GetValue(_humidityToLocation!, humidity);
-
-        _iterations++;
+        var soil = GetDestinationDebug(_seedToSoil!, seed, "_seedToSoil");
+        var fertilizer = GetDestinationDebug(_soilToFertilizer!, soil, "_soilToFertilizer");
+        var water = GetDestinationDebug(_fertilizerToWater!, fertilizer, "_fertilizerToWater");
+        var light = GetDestinationDebug(_waterToLight!, water, "_waterToLight");
+        var temperature = GetDestinationDebug(_lightToTemperature!, light, "_lightToTemperature");
+        var humidity = GetDestinationDebug(_temperatureToHumidity!, temperature, "_temperatureToHumidity");
+        var location = GetDestinationDebug(_humidityToLocation!, humidity, "_humidityToLocation");
         return location;
     }
-    
-    private static long GetValue(Map map, long value)
-    {
-        if (!map.InRange(value))
-            return value;
 
-        var submap = map.AdaptedBinarySearchCorrectMap(value);
+    private static long GetDestinationDebug(Map map, long value, string name)
+    {
+        var before = value;
+        var after = GetDestination(map, value);
+        Console.WriteLine($"{name}: {before} => {after}");
+        return after;
+    }
+    
+    private static long GetDestination(Map map, long source)
+    {
+        if (!map.InRange(source))
+            return source;
+
+        var submap = map.OrderedSubMaps.FirstOrDefault(osm => osm.InBounds(source));
         if (submap == null)
-            return value;
+            return source;
         
-        var (inRange, result) = submap.MapToDestination(value);
+        var (inRange, result) = submap.MapToDestination(source);
 
         if (!inRange) throw new Exception("Incorrect submap");
 
         return result;
     }
     
-    private static void AssertEqual<T>(T expected, T value)
+    private static long GetSourceDebug(Map map, long destination, string name)
     {
-        if (!value.Equals(expected))
-            throw new Exception($"got: {value}, expected: {expected}");
+        var before = destination;
+        var after = GetSource(map, destination);
+        //Console.WriteLine($"{name}: {before} => {after}");
+        return after;
+    }
+    
+    private static long GetSource(Map map, long destination)
+    {
+        foreach (var el in map.OrderedSubMaps)
+        {
+            if (el.MinimumDestination <= destination && destination <= el.MaximumDestination)
+            {
+                var (_, result) = el.MapToSource(destination);
+                return result;
+            }
+        }
+        
+        // nothing found, return destination itself (ASSUMING THAT IS ALLRIGHT THE OTHER WAY AROUND WHICH IS WHAT I DID)
+        return destination;
     }
 }
---
+++ 2023/C#/AdventOfCode2023/helpers/Point3D.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2023.helpers;
+
+public class Point3D : IEquatable<Point3D>
+{
+    public Point3D(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public Point3D(Point3D source)
+    {
+        X = source.X;
+        Y = source.Y;
+        Z = source.Z;
+    }
+
+    public readonly int X, Y, Z;
+    
+    public override string ToString() => $"{X},{Y},{Z}";
+    public static Point3D operator +(Point3D a, Point3D b) => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    public static Point3D operator -(Point3D a, Point3D b) => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+    #region IEquatable
+    
+    public bool Equals(Point3D? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return X == other.X && Y == other.Y && Z == other.Z;
+    }
+
+    public override int GetHashCode() => HashCode.Combine(X, Y, Z);
+
+    public override bool Equals(object? obj) => Equals(obj as Point3D);
+    
+    #endregion
+}

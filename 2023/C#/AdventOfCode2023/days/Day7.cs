50f01559a7ccc8bba106f59776738025d0da710a
Julian Declercq <julian.declercq1@gmail.com>
2023-12-07 18:10:29 +0100

    2023 day 7 refactor

--- 2023/C#/AdventOfCode2023/days/Day7.cs
+++ 2023/C#/AdventOfCode2023/days/Day7.cs
@@ -17,7 +17,7 @@ public static class Day7
         
         var hands = File.ReadAllLines("../../../input/Day7.txt").Select(LineToHand).ToList();
         hands.Sort(HandCompare);
-
+        
         ulong answer = 0;
         for (var i = 0; i < hands.Count; ++i)
         {
@@ -56,7 +56,7 @@ public static class Day7
             3 => occurrences.Any(o => o.Value == 2)
                 ? (ulong)10_000_000_000 // full house
                 : (ulong)1_000_000_000, // three of a kind
-            2 => occurrences.Where(o => o.Key != highestOccurence.Key).Any(o => o.Value == 2)
+            2 => occurrences.Any(o => o.Key != highestOccurence.Key && o.Value == 2)
                 ? (ulong)100_000_000 // two pair
                 : (ulong)10_000_000, // pair
             1 => 1_000_000, // high card
@@ -66,14 +66,8 @@ public static class Day7
 
     private static readonly Dictionary<char, int> CardValues = new()
     {
-        ['A'] = 14, ['K'] = 13, ['Q'] = 12, ['J'] = 11, ['T'] = 10,
-        ['9'] = 9, ['8'] = 8, ['7'] = 7, ['6'] = 6, ['5'] = 5, ['4'] = 4, ['3'] = 3, ['2'] = 2
-    };
-    
-    private static readonly Dictionary<char, int> JokerCardValues = new()
-    {
         ['A'] = 14, ['K'] = 13, ['Q'] = 12, ['T'] = 10,
-        ['9'] = 9, ['8'] = 8, ['7'] = 7, ['6'] = 6, ['5'] = 5, ['4'] = 4, ['3'] = 3, ['2'] = 2, ['J'] = 1
+        ['9'] = 9, ['8'] = 8, ['7'] = 7, ['6'] = 6, ['5'] = 5, ['4'] = 4, ['3'] = 3, ['2'] = 2
     };
     
     private static string JokerTransform(string cards)
@@ -106,13 +100,15 @@ public static class Day7
         if (lhsTypeScore > rhsTypeScore) return 1;
         if (lhsTypeScore < rhsTypeScore) return -1;
 
-        var lookup = _part1 ? CardValues : JokerCardValues;
         for (var i = 0; i < lhs.Cards.Length; ++i)
         {
-            if (lookup[lhs.Cards[i]] > lookup[rhs.Cards[i]])
+            var lhsValue = lhs.Cards[i] == 'J' ? (_part1 ? 11 : 1) : CardValues[lhs.Cards[i]];
+            var rhsValue = rhs.Cards[i] == 'J' ? (_part1 ? 11 : 1) : CardValues[rhs.Cards[i]];
+
+            if (lhsValue > rhsValue)
                 return 1;
             
-            if (lookup[lhs.Cards[i]] < lookup[rhs.Cards[i]])
+            if (lhsValue < rhsValue)
                 return -1;
         }
 

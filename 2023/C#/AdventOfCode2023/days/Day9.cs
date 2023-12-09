849e51a7fd04bad85b9ddfc7331c973d2a0cbaea
Julian Declercq <julian.declercq1@gmail.com>
2023-12-09 20:41:50 +0100

    2023 day 9 part 1

---
+++ 2023/C#/AdventOfCode2023/days/Day9.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode2023.days;
+
+public class Day9
+{
+    public void Part1()
+    {
+        var input = File.ReadAllLines("../../../input/Day9.txt");
+        var answer = 0;
+        foreach (var set in input)
+        {
+            var diffs = set.Split(' ').Select(int.Parse).ToList();
+            var saved = new List<List<int>> { diffs };
+
+            while (diffs.Any(d => !d.Equals(0)))
+            {
+                diffs = CalcDiffs(diffs);
+                saved.Add(diffs);
+            }
+
+            saved.Reverse(); // bottom up
+
+            for (var i = 0; i < saved.Count - 1; ++i)
+                saved[i + 1].Add(saved[i + 1].Last() + saved[i].Last());
+
+            answer += saved.Last().Last();
+        }
+        Console.WriteLine(answer);
+    }
+
+    private static List<int> CalcDiffs(List<int> input)
+    {
+        var diffs = new List<int>();
+        for (var i = 0; i < input.Count - 1; ++i)
+            diffs.Add(input[i + 1] - input[i]);
+
+        return diffs;
+    }
+}

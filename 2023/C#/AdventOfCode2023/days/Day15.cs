92297c1a2902639918f28c67872993a40ab9e4f7
Julian Declercq <julian.declercq1@gmail.com>
2023-12-15 21:41:12 +0100

    2023 day 15 part 2

--- 2023/C#/AdventOfCode2023/days/Day15.cs
+++ 2023/C#/AdventOfCode2023/days/Day15.cs
@@ -1,11 +1,76 @@
-﻿namespace AdventOfCode2023.days;
+﻿using System.Text.RegularExpressions;
+using AdventOfCode2023.helpers;
 
-public class Day15
+namespace AdventOfCode2023.days;
+
+public partial class Day15
 {
+    private record Lens(string Label, int FocalLength);
+
     public void Solve()
     {
+        var helper = new RegexHelper(Pattern(), "label", "operation", "focallength");
         var input = File.ReadAllLines("../../../input/Day15.txt").First().Split(',');
+        var boxes = new Dictionary<int, List<Lens>>();
+        foreach (var line in input)
+        {
+            if (!helper.Match(line))
+                throw new Exception($"Couldn't match {line}");
+
+            var label = helper.Get("label");
+            var operation = helper.Get("operation").First();
+
+            switch (operation)
+            {
+                case '-':
+                {
+                    if (boxes.TryGetValue(Hash(label), out var lenses))
+                    {
+                        var idx = lenses.FindIndex(l => l.Label.Equals(label));
+                        if (idx != -1)
+                            lenses.RemoveAt(idx);
+                    }
+                    break;
+                }
+                case '=':
+                {
+                    var focalLength = helper.GetInt("focallength");
+                    var labelHash = Hash(label);
+                    if (boxes.TryGetValue(labelHash, out var lenses))
+                    {
+                        var idx = lenses.FindIndex(l => l.Label.Equals(label));
+                        if (idx != -1)
+                        {
+                            lenses[idx] = lenses[idx] with { FocalLength = focalLength };
+                        }
+                        else
+                        {
+                            lenses.Add(new Lens(label, focalLength));
+                        }
+                    }
+                    else
+                    {
+                        boxes[labelHash] = new List<Lens> { new(label, focalLength) };
+                    }
+                    break;
+                }
+            }
+        }
+
+        var configurationFocusingPower = 0;
+        foreach (var (hash, lenses) in boxes)
+        {
+            for (var i = 0; i < lenses.Count; ++i)
+            {
+                var focusingPower = 1 + hash;
+                focusingPower *= i + 1;
+                focusingPower *= lenses[i].FocalLength;
+                configurationFocusingPower += focusingPower;
+            }
+        }
+        
         Console.WriteLine(input.Sum(Hash));
+        Console.WriteLine(configurationFocusingPower);
     }
 
     private static int Hash(string input)
@@ -20,4 +85,7 @@ public class Day15
 
         return hash;
     }
+
+    [GeneratedRegex(@"(\w+)([=-])(\d+)?")]
+    private static partial Regex Pattern();
 }

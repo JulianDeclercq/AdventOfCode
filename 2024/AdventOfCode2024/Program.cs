c75c3ef480e1375b332511d1e29a79cee8056af1
Julian Declercq <julian.declercq1@gmail.com>
2024-12-01 10:34:22 +0100

    2024 day 1 part 1 and gitignore

---
+++ 2024/AdventOfCode2024/Program.cs
@@ -0,0 +1,11 @@
+﻿var lines = File.ReadAllLines("input/day1.txt")
+    .Select(l => l.Split(' ')
+        .Where(l2 => !string.IsNullOrWhiteSpace(l2))
+        .Select(int.Parse)
+        .ToArray())
+    .ToArray();
+
+var lhs = lines.Select(l => l.First()).Order();
+var rhs = lines.Select(l => l.Last()).Order();
+var answer = lhs.Zip(rhs).Aggregate(0, (total, next) => total + Math.Abs(next.First - next.Second));
+Console.WriteLine(answer);

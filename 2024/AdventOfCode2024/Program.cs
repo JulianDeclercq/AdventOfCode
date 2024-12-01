af2f51563e59e6b204049e956511078a6df7beea
Julian Declercq <julian.declercq1@gmail.com>
2024-12-01 10:40:21 +0100

    2024 day 1 part 2

--- 2024/AdventOfCode2024/Program.cs
+++ 2024/AdventOfCode2024/Program.cs
@@ -1,11 +1,13 @@
 ﻿var lines = File.ReadAllLines("input/day1.txt")
-    .Select(l => l.Split(' ')
-        .Where(l2 => !string.IsNullOrWhiteSpace(l2))
+    .Select(l => l.Split("   ")
         .Select(int.Parse)
         .ToArray())
     .ToArray();
 
-var lhs = lines.Select(l => l.First()).Order();
-var rhs = lines.Select(l => l.Last()).Order();
-var answer = lhs.Zip(rhs).Aggregate(0, (total, next) => total + Math.Abs(next.First - next.Second));
-Console.WriteLine(answer);
+var lhs = lines.Select(l => l.First()).Order().ToArray();
+var rhs = lines.Select(l => l.Last()).Order().ToArray();
+var part1 = lhs.Zip(rhs).Aggregate(0, (total, next) => total + Math.Abs(next.First - next.Second));
+Console.WriteLine(part1);
+
+var part2 = lhs.Sum(l => l * rhs.Count(r => r == l));
+Console.WriteLine(part2);

4cf6dbebe5ced005092779fdcd34f6684fa91d5b
Julian Declercq <julian.declercq1@gmail.com>
2024-12-02 19:08:25 +0100

    2024 day 2 part 1

--- 2024/AdventOfCode2024/Day2.cs
+++ 2024/AdventOfCode2024/Day2.cs
@@ -4,6 +4,42 @@ public class Day2
 {
     public static void Solve()
     {
-        
+        var reports = File.ReadAllLines("input/day2.txt")
+            .Select(s => s.Split(' ')
+                .Select(int.Parse)
+                .ToArray())
+            .ToArray();
+
+        var answer = reports.Count(IsValidReport);
+        Console.WriteLine(answer);
+    }
+
+    private static bool IsValidReportAscending(int[] report)
+    {
+        for (var i = 0; i < report.Length - 1; i++)
+        {
+            var diff = report[i + 1] - report[i];
+            if (diff is < 1 or > 3)
+                return false;
+        }
+
+        return true;
+    }
+    
+    private static bool IsValidReportDescending(int[] report)
+    {
+        for (var i = 0; i < report.Length - 1; i++)
+        {
+            var diff = report[i] - report[i + 1];
+            if (diff is < 1 or > 3)
+                return false;
+        }
+
+        return true;
+    }
+    
+    private static bool IsValidReport(int[] report)
+    {
+        return IsValidReportAscending(report) || IsValidReportDescending(report);
     }
 }
--- 2024/AdventOfCode2024/Program.cs
+++ 2024/AdventOfCode2024/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode2024;
 
-Day1.Solve();
+Day2.Solve();

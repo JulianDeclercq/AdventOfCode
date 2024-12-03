209bdf0581e238c4eb88f1a37be687bcf7ed9923
Julian Declercq <julian@geoguessr.com>
2024-12-03 10:00:15 +0100

    2024 day 3 part 1

--- 2024/AdventOfCode2024/Day3.cs
+++ 2024/AdventOfCode2024/Day3.cs
@@ -9,11 +9,9 @@ public class Day3
         var lines = File.ReadAllLines("input/day3.txt");
         var input = string.Join("", lines);
         var regex = new Regex(@"mul\((\d+),(\d+)\)");
-        var matches = regex.Matches(input);
 
-        var answer = 0;
-        foreach (Match match in matches)
-            answer += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value) ;
+        var answer = regex.Matches(input).Aggregate(0, (total, next) =>
+            total + int.Parse(next.Groups[1].Value) * int.Parse(next.Groups[2].Value));
         
         Console.WriteLine(answer);
     }

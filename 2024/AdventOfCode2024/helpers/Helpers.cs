e7a751ae7d9f4d2271077bfca7b297f7c44a800a
Julian Declercq <julian.declercq1@gmail.com>
2024-12-04 22:33:08 +0100

    added helpers

---
+++ 2024/AdventOfCode2024/helpers/Helpers.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode2024.helpers;
+
+public static class Helpers
+{
+    public static int ToInt(char c) => int.Parse(c.ToString());
+
+    public static bool AllSmallerThan(this IEnumerable<int> collection, int target)
+    {
+        return collection.All(x => x < target);
+    }
+    
+    public static void AssertEqual<T>(T expected, T value)
+    {
+        if (!value.Equals(expected))
+            throw new Exception($"got: {value}, expected: {expected}");
+    }
+
+    public static Direction CalculateDirection(Point from, Point to)
+    {
+        return (to - from) switch
+        {
+            { X: 0, Y: -1 } => Direction.North,
+            { X: 1, Y: 0 } => Direction.East,
+            { X: 0, Y: 1 } => Direction.South,
+            { X: -1, Y: 0 } => Direction.West,
+            _ => throw new Exception("Couldn't determine direction")
+        };
+    }
+}
+
+public enum Direction
+{
+    None = 0,
+    North = 1,
+    East = 2,
+    South = 3,
+    West = 4
+}
---
+++ 2024/AdventOfCode2024/helpers/Point.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode2024.helpers;
+
+public class Point : IEquatable<Point>
+{
+    private static Point Normalized(Point p)
+    {
+        var distance = Math.Sqrt(p.X * p.X + p.Y * p.Y);
+        return new Point(Convert.ToInt16(p.X / distance), Convert.ToInt16(p.Y / distance));
+    }
+    public static Point Direction(Point from, Point to) => Normalized(to - from);
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point(Point source)
+    {
+        X = source.X;
+        Y = source.Y;
+    }
+
+    public readonly int X, Y;
+    
+    public override string ToString() => $"{X},{Y}";
+    public static Point operator +(Point a, Point b) => new(a.X + b.X, a.Y + b.Y);
+    public static Point operator -(Point a, Point b) => new(a.X - b.X, a.Y - b.Y);
+
+    #region IEquatable
+    
+    public bool Equals(Point? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return X == other.X && Y == other.Y;
+    }
+
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+
+    public override bool Equals(object? obj) => Equals(obj as Point);
+    
+    #endregion
+}

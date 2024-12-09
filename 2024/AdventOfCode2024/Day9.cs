4e5899411138a0b583c5c93db100a0a39633d84c
Julian Declercq <julian.declercq1@gmail.com>
2024-12-09 18:14:44 +0100

    preparing for part 2

--- 2024/AdventOfCode2024/Day9.cs
+++ 2024/AdventOfCode2024/Day9.cs
@@ -2,17 +2,18 @@ namespace AdventOfCode2024;
 
 public class Day9
 {
-    public static void Solve()
+    private const char Empty = '.'; 
+    public static void Solve(int part)
     {
+        if (part != 1 && part != 2)
+            throw new Exception($"Invalid part {part}");
+        
         var input =
-            File
-            .ReadAllLines("input/day9.txt")
-            .Single()
-            // "2333133121414131402"
+            // File.ReadAllLines("input/day9.txt").Single()
+            "2333133121414131402"
             .Select(c => (int)char.GetNumericValue(c))
             .ToArray();
         
-        const char empty = '.'; 
         List<char> disk = [];
 
         // Load
@@ -21,7 +22,7 @@ public class Day9
         foreach (var number in input)
         {
             for (var i = 0; i < number; ++i)
-                disk.Add(file ? (char)(id + '0') : empty);
+                disk.Add(file ? (char)(id + '0') : Empty);
 
             if (file)
                 id++;
@@ -30,36 +31,19 @@ public class Day9
         }
         
         // Process
-        var lastIndexTaken = disk.Count;
-        for (var i = 0; i < disk.Count; ++i)
+        if (part is 1)
         {
-            // Console.WriteLine(string.Join("", disk));
-            
-            // break if we've reached the last thing we've taken, this means all empty spots up until now have been filled
-            if (lastIndexTaken <= i)
-                break;
-
-            if (disk[i] is not empty)
-                continue;
-            
-            // find the last non-empty character
-            // for (var j = disk.Count - 1; j > i; --j)
-            for (var j = lastIndexTaken - 1; j > i; --j)
-            {
-                if (disk[j] is not empty)
-                {
-                    lastIndexTaken = j;
-                    disk[i] = disk[lastIndexTaken];
-                    disk[lastIndexTaken] = '.';
-                    break;
-                }
-            }
+            Part1(disk);
+        }
+        else
+        {
+            Part2(disk);
         }
 
         long checksum = 0;
         for (var i = 0; i < disk.Count; ++i)
         {
-            if (disk[i] is empty)
+            if (disk[i] is Empty)
                 continue;
             
             var lhs = i;
@@ -69,5 +53,63 @@ public class Day9
         }
         
         Console.WriteLine(checksum);
+    }
+
+    private static void Part1(List<char> disk)
+    {
+        var lastIndexTaken = disk.Count;
+        for (var i = 0; i < disk.Count; ++i)
+        {
+            // Console.WriteLine(string.Join("", disk));
+            
+            // break if we've reached the last thing we've taken, this means all empty spots up until now have been filled
+            if (lastIndexTaken <= i)
+                break;
+
+            if (disk[i] is not Empty)
+                continue;
+            
+            // find the last non-empty character
+            // for (var j = disk.Count - 1; j > i; --j)
+            for (var j = lastIndexTaken - 1; j > i; --j)
+            {
+                if (disk[j] is not Empty)
+                {
+                    lastIndexTaken = j;
+                    disk[i] = disk[lastIndexTaken];
+                    disk[lastIndexTaken] = '.';
+                    break;
+                }
+            }
+        }
+    }
+    
+    private static void Part2(List<char> disk)
+    {
+        var lastIndexTaken = disk.Count;
+        for (var i = 0; i < disk.Count; ++i)
+        {
+            // Console.WriteLine(string.Join("", disk));
+            
+            // break if we've reached the last thing we've taken, this means all empty spots up until now have been filled
+            if (lastIndexTaken <= i)
+                break;
+
+            if (disk[i] is not Empty)
+                continue;
+            
+            // find the last non-empty character
+            // for (var j = disk.Count - 1; j > i; --j)
+            for (var j = lastIndexTaken - 1; j > i; --j)
+            {
+                if (disk[j] is not Empty)
+                {
+                    lastIndexTaken = j;
+                    disk[i] = disk[lastIndexTaken];
+                    disk[lastIndexTaken] = '.';
+                    break;
+                }
+            }
+        }
     }
 }
--- 2024/AdventOfCode2024/Program.cs
+++ 2024/AdventOfCode2024/Program.cs
@@ -2,7 +2,7 @@
 
 var watch = System.Diagnostics.Stopwatch.StartNew();
 
-Day9.Solve();
+Day9.Solve(part: 1);
 
 watch.Stop();
 Console.WriteLine(watch.Elapsed);

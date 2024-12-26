ed89d22aa7832960a49679da948ae69d97064f59
Julian Declercq <julian.declercq1@gmail.com>
2024-12-26 16:33:20 +0100

    2024 day 5

--- 2024/AdventOfCode2024/Day5.cs
+++ 2024/AdventOfCode2024/Day5.cs
@@ -10,59 +10,56 @@ public class Day5
         var lines = File.ReadAllLines("input/day5.txt");
         var rules = lines.TakeWhile(l => !string.IsNullOrWhiteSpace(l)).ToArray();
 
-        var lookup = new Dictionary<int, List<int>>();
+        Dictionary<int, List<int>> lookup = [], reverseLookup = [];
         foreach (var rule in rules)
         {
             var parts = rule.Split('|').Select(int.Parse).ToArray();
             var target = lookup.TryGetValue(parts[0], out var list) ? list : [];
             target.Add(parts[1]);
             lookup[parts[0]] = target;
-        }
-
-        var reverseLookup = new Dictionary<int, List<int>>();
-        foreach (var rule in rules)
-        {
-            var parts = rule.Split('|').Select(int.Parse).ToArray();
-            var target = reverseLookup.TryGetValue(parts[1], out var list) ? list : [];
+            
+            target = reverseLookup.TryGetValue(parts[1], out var list2) ? list2 : [];
             target.Add(parts[0]);
             reverseLookup[parts[1]] = target;
         }
         
-        var pages = lines.Skip(rules.Length + 1)
+        var updates = lines.Skip(rules.Length + 1)
             .Select(l => l
                 .Split(',')
                 .Select(int.Parse)
                 .ToArray())
             .ToArray();
 
-        List<int[]> validPages = [];
-        foreach (var page in pages)
+        List<int[]> validUpdates = [];
+        List<int[]> invalidUpdates = [];
+        foreach (var update in updates)
         {
-            var correct = true;
+            var valid = true;
             
-            for (var j = 0; j < page.Length; ++j)
+            for (var j = 0; j < update.Length; ++j)
             {
-                if (!reverseLookup.TryGetValue(page[j], out var valuesThatShouldComeBefore))
+                if (!reverseLookup.TryGetValue(update[j], out var shouldComeBefore))
                     continue;
                 
-                var valuesThatComeAfter = page.Skip(j + 1).ToHashSet();
-                if (valuesThatShouldComeBefore.Any(v => valuesThatComeAfter.Contains(v)))
+                var comeAfter = update.Skip(j + 1).ToHashSet();
+                if (shouldComeBefore.Any(x => comeAfter.Contains(x)))
                 {
-                    correct = false;
+                    valid = false;
                     break;
                 }
             }
-            
-            if (correct)
-                validPages.Add(page);
+
+            var target = valid ? validUpdates : invalidUpdates;
+            target.Add(update);
         }
 
         var answer = 0;
-        foreach (var page in validPages)
+        foreach (var update in validUpdates)
         {
-            answer += page[page.Length / 2];
+            answer += update[update.Length / 2];
             // Console.WriteLine(string.Join(",", page));
         }
         Console.WriteLine(answer);
     }
+
 }

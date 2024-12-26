9d9084cc0111e32efc8afa245e48efe2d313fb46
Julian Declercq <julian.declercq1@gmail.com>
2024-12-26 17:10:24 +0100

    2024 day 5 part 2 wip

--- 2024/AdventOfCode2024/Day5.cs
+++ 2024/AdventOfCode2024/Day5.cs
@@ -23,6 +23,9 @@ public class Day5
             reverseLookup[parts[1]] = target;
         }
         
+        // var masterOrder = Sort(lookup.Keys.Concat(reverseLookup.Keys).ToHashSet().ToList(), lookup);
+        // Console.WriteLine($"MASTERORDER {string.Join(",", masterOrder)}");
+        
         var updates = lines.Skip(rules.Length + 1)
             .Select(l => l
                 .Split(',')
@@ -30,28 +33,8 @@ public class Day5
                 .ToArray())
             .ToArray();
 
-        List<int[]> validUpdates = [];
-        List<int[]> invalidUpdates = [];
-        foreach (var update in updates)
-        {
-            var valid = true;
-            
-            for (var j = 0; j < update.Length; ++j)
-            {
-                if (!reverseLookup.TryGetValue(update[j], out var shouldComeBefore))
-                    continue;
-                
-                var comeAfter = update.Skip(j + 1).ToHashSet();
-                if (shouldComeBefore.Any(x => comeAfter.Contains(x)))
-                {
-                    valid = false;
-                    break;
-                }
-            }
-
-            var target = valid ? validUpdates : invalidUpdates;
-            target.Add(update);
-        }
+        var validUpdates = updates.Where(u => IsValid(u, reverseLookup)).ToList();
+        var invalidUpdates = updates.Where(u => !IsValid(u, reverseLookup)).ToList();
 
         var answer = 0;
         foreach (var update in validUpdates)
@@ -62,4 +45,46 @@ public class Day5
         Console.WriteLine(answer);
     }
 
+    private static bool IsValid(int[] update, Dictionary<int, List<int>> reverseLookup)
+    {
+        for (var j = 0; j < update.Length; ++j)
+        {
+            if (!reverseLookup.TryGetValue(update[j], out var shouldComeBefore))
+                continue;
+            
+            var comeAfter = update.Skip(j + 1).ToHashSet();
+            if (shouldComeBefore.Any(x => comeAfter.Contains(x)))
+                return false;
+        }
+
+        return true;
+    }
+
+    private static List<int> Sort(List<int> current, Dictionary<int, List<int>> lookup)
+    {
+        for (var i = 0; i < current.Count; ++i)
+        {
+            var page = current[i];
+            if (!lookup.TryGetValue(page, out var pageShouldComeBefore))
+                continue;
+
+            var numbersBefore = current.Take(i).ToList();
+            var idx = numbersBefore.FindIndex(x => pageShouldComeBefore.Contains(x));
+            if (idx == -1)
+                continue;
+            
+            Console.WriteLine($"Found invalid page {numbersBefore[idx]} ({idx}) that came " +
+                              $"before page {current[i]} ({i}) but should be after.");
+
+            Console.WriteLine($"BEFORE {string.Join(',', current)}");
+            current.Remove(page);
+            current.Insert(idx, page); 
+            Console.WriteLine($"AFTER {string.Join(',', current)}");
+            return Sort(current, lookup);
+        }
+
+        // not sure
+        Console.WriteLine("DONE SORTING");
+        return current;
+    }
 }

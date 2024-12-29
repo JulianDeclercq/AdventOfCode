3cd85db65993443a490e2b765c0bae7dcf9950d2
Julian Declercq <julian.declercq1@gmail.com>
2024-12-29 21:50:10 +0100

    2024 day 7 part 2

--- 2024/AdventOfCode2024/Day7.cs
+++ 2024/AdventOfCode2024/Day7.cs
@@ -11,6 +11,13 @@ public class Day7
             throw new Exception($"Invalid part {part}");
                 
         var lines = File.ReadAllLines("input/day7.txt");
+        // string[] lines =
+        // [
+        //     // "156: 15 6",
+        //     // "7290: 6 8 6 15",
+        //     // "192: 17 8 14"
+        //     // "100: 50 50 2"
+        // ];
         var helper = new RegexHelper(new Regex(@"(\d+): (.+)"), "result", "operands");
         long answer = 0;
         
@@ -21,53 +28,31 @@ public class Day7
 
             var result = helper.GetLong("result");
             var operands = helper.Get("operands").Split(' ').Select(long.Parse);
-            if (IsValid(result, operands.ToArray()))
+            if (IsValid(result, operands.ToArray(), part))
                 answer += result;
         }
         Console.WriteLine(answer);
     }
 
-    private static bool IsValid(long result, long[] operands)
+    private static bool IsValid(long result, long[] operands, int part)
     {
         // I guess the last operation here is a bit fake, but it works for the logic
         if (Step(operands[0], result, operands[1..], OperandType.Addition,
                 new Operation(operands[0], OperandType.Addition)))
             return true;
-
+        
         if (Step(operands[0], result, operands[1..], OperandType.Multiplication,
                 new Operation(operands[0], OperandType.Multiplication)))
             return true;
         
-        // if (Step(operands[0], result, operands[1..], OperandType.Concatenation,
-        //         new Operation(operands[0], OperandType.Concatenation)))
-        //     return true;
-        
-        return false;
-    }
-    
-    
-    private static bool Stepx(long current, long result, long[] operands, bool add)
-    {
-        if (operands.Length == 0)
+        if (part is 1)
             return false;
-
-        if (add)
-            current += operands[0];
-        else
-            current *= operands[0];
         
-        if (current > result) // early return
-            return false;
-
-        if (current == result && operands.Length == 1)
-            return true;
-
-        if (Stepx(current, result, operands[1..], true))
+        // not sure about the first current, see other comment here too
+        if (Step(operands[0], result, operands[1..], OperandType.Concatenation,
+                new Operation(operands[0], OperandType.Addition))) // first operation needs to be addition to not make it count
             return true;
         
-        if (Stepx(current, result, operands[1..], false))
-            return true;
-
         return false;
     }
     
@@ -77,6 +62,7 @@ public class Day7
             return false;
         
         var currentOperand = operands[0];
+        var (lastOperand, lastOperationType) = lastOperation;
         switch (type)
         {
             case OperandType.Addition:
@@ -86,6 +72,29 @@ public class Day7
                 current *= currentOperand;
                 break;
             case OperandType.Concatenation:
+                // undo the last operation
+                switch (lastOperationType)
+                {
+                    case OperandType.Addition:
+                        // current -= lastOperand;
+                        break;
+                    case OperandType.Multiplication:
+                        // current /= lastOperand;
+                        break;
+                    case OperandType.Concatenation: 
+                        break; // do nothing
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                var concatenated = long.Parse($"{current}{currentOperand}");
+                // Console.WriteLine($"Concatenated {current} and {currentOperand} into {concatenated}");
+                current = concatenated; // TODO: NOT SURE
+                // if (concatenated is 486)
+                // {
+                //     int kpbt = 5;
+                // }
+                // currentOperand = concatenated;
                 break;
             default: throw new ArgumentOutOfRangeException(nameof(type), type, null);
         }
@@ -95,7 +104,7 @@ public class Day7
 
         if (current == result && operands.Length == 1)
             return true;
-
+        
         var thisOperation = new Operation(currentOperand, type);
         if (Step(current, result, operands[1..], OperandType.Addition, thisOperation))
             return true;

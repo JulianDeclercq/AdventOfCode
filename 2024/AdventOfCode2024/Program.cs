2cfcf6260f83df6ab5cfd73d1d4f26f208478454
Julian Declercq <julian.declercq1@gmail.com>
2025-03-29 22:11:55 +0100

    day 9 part 2, woohoo!

--- 2024/AdventOfCode2024/Program.cs
+++ 2024/AdventOfCode2024/Program.cs
@@ -2,9 +2,8 @@
 
 var watch = System.Diagnostics.Stopwatch.StartNew();
 
+Day9.Solve(part: 1);
 Day9.Solve(part: 2);
 
 watch.Stop();
 Console.WriteLine(watch.Elapsed);
-
-// "9953877292941" // checksum: 5768, final disk state: 00000000063333333.11111...22222222................444444444..555555555.....

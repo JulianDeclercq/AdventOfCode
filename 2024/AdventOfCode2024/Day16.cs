ae4d56517189949143021215520069e08cbfb406
Julian Declercq <julian.declercq1@gmail.com>
2025-05-01 21:53:41 +0200

    day 16 traverse all valid neighbours

--- 2024/AdventOfCode2024/Day16.cs
+++ 2024/AdventOfCode2024/Day16.cs
@@ -7,6 +7,8 @@ public class Day16
     private Grid<char> _grid;
     public const char Wall = '#';
     public const char End = 'E';
+    public const char Start = 'E';
+    private const Direction InitialDirection = Direction.East;
     private List<int> _scores = [];
 
     public Day16(string filePath)
@@ -16,6 +18,13 @@ public class Day16
         Console.WriteLine(_grid);
     }
 
+    public void Solve()
+    {
+        var start = _grid.AllExtended().Single(x => x.Value is Start);
+        Step(start.Position, InitialDirection, 0, []);
+        Console.WriteLine($"Answer {_scores.Max()}");
+    }
+
     private static bool CanMoveTo(GridElement<char>? neighbour, HashSet<Point> visited)
     {
         return neighbour is not null && neighbour.Value is not Wall && !visited.Contains(neighbour.Position);
@@ -23,30 +32,28 @@ public class Day16
 
     private void Step(Point position, Direction direction, int score, HashSet<Point> visited)
     {
-        var visitedCopy = visited.ToHashSet();
-
-        // check easiest route first (for no good reason tho lmao)
-        var easyNeighbour = _grid.GetNeighbour(position, direction);
-        if (CanMoveTo(easyNeighbour, visitedCopy))
-        {
-            score++;
-            visitedCopy.Add(easyNeighbour!.Position);
-            position += Helpers.DirectionToPoint(direction);
-
-            if (easyNeighbour.Value is End)
-            {
-                _scores.Add(score);
-                return;
-            }
-            
-            Step(position, direction, score, visitedCopy);
-            return;
-        }
-
-        foreach (var neighbour in _grid.NeighboursExtended(position).Where(n => n.Value is not Wall))
+        var neighbours = _grid.NeighboursExtended(position, includeDiagonals: false);
+        foreach (var neighbour in neighbours.Where(n => n.Value is not Wall))
         {
             if (visited.Contains(neighbour.Position))
                 continue;
+            
+            var visitedCopy = visited.ToHashSet();
+            if (CanMoveTo(neighbour, visitedCopy))
+            {
+                score++;
+                visitedCopy.Add(neighbour.Position);
+                position += Helpers.DirectionToPoint(direction);
+
+                if (neighbour.Value is End)
+                {
+                    _scores.Add(score);
+                    return;
+                }
+
+                Step(position, direction, score, visitedCopy);
+                return;
+            }
         }
     }
 }

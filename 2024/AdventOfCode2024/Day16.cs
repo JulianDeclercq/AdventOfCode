8027845ae45c9735fe999b7596b98829ae6d4dfc
Julian Declercq <julian.declercq1@gmail.com>
2025-05-01 22:27:15 +0200

    day 16 added path and debug logging

--- 2024/AdventOfCode2024/Day16.cs
+++ 2024/AdventOfCode2024/Day16.cs
@@ -4,12 +4,28 @@ namespace AdventOfCode2024;
 
 public class Day16
 {
+    public class Path
+    {
+        public HashSet<Point> Visited { get; init; }
+        public int Score { get; init; }
+
+        public string VisualiseOnGrid(Grid<char> grid)
+        {
+            var copy = grid.ShallowCopy();
+            
+            foreach (var point in Visited)
+                copy.Set(point, '@');
+            
+            return copy.ToString();
+        }
+    }
+
     private Grid<char> _grid;
     public const char Wall = '#';
     public const char End = 'E';
-    public const char Start = 'E';
+    public const char Start = 'S';
     private const Direction InitialDirection = Direction.East;
-    private List<int> _scores = [];
+    private List<Path> _paths = [];
 
     public Day16(string filePath)
     {
@@ -21,8 +37,10 @@ public class Day16
     public void Solve()
     {
         var start = _grid.AllExtended().Single(x => x.Value is Start);
-        Step(start.Position, InitialDirection, 0, []);
-        Console.WriteLine($"Answer {_scores.Max()}");
+        Step(start.Position, InitialDirection, 0, [start.Position]);
+
+        var winner = _paths.MinBy(p => p.Score);
+        Console.WriteLine($"Answer\n{winner!.VisualiseOnGrid(_grid)}");
     }
 
     private static bool CanMoveTo(GridElement<char>? neighbour, HashSet<Point> visited)
@@ -30,14 +48,47 @@ public class Day16
         return neighbour is not null && neighbour.Value is not Wall && !visited.Contains(neighbour.Position);
     }
 
+    // check if the target direction is within 90 degrees
+    private static Direction? TryRotateTowards(Direction current, Direction target)
+    {
+        var canRotate = current switch
+        {
+            Direction.North when target is Direction.East => true,
+            Direction.North when target is Direction.West => true,
+            Direction.East when target is Direction.North => true,
+            Direction.East when target is Direction.South => true,
+            Direction.South when target is Direction.East => true,
+            Direction.South when target is Direction.West => true,
+            Direction.West when target is Direction.South => true,
+            Direction.West when target is Direction.North => true,
+            _ => false
+        };
+
+        return canRotate ? target : null;
+    }
+
     private void Step(Point position, Direction direction, int score, HashSet<Point> visited)
     {
-        var neighbours = _grid.NeighboursExtended(position, includeDiagonals: false);
-        foreach (var neighbour in neighbours.Where(n => n.Value is not Wall))
+        var neighbours = _grid.NeighboursExtended(position, includeDiagonals: false)
+            .Where(n => n.Value is not Wall)
+            .ToArray();
+
+        foreach (var neighbour in neighbours)
         {
             if (visited.Contains(neighbour.Position))
                 continue;
-            
+
+            var neighbourDirection = Helpers.CalculateDirection(position, neighbour.Position);
+            if (neighbourDirection != direction)
+            {
+                var target = TryRotateTowards(direction, neighbourDirection);
+                if (target is not null)
+                {
+                    score += 1000;
+                    Step(position, target.Value, score, visited.ToHashSet());
+                }
+            }
+
             var visitedCopy = visited.ToHashSet();
             if (CanMoveTo(neighbour, visitedCopy))
             {
@@ -47,7 +98,11 @@ public class Day16
 
                 if (neighbour.Value is End)
                 {
-                    _scores.Add(score);
+                    _paths.Add(new Path
+                    {
+                        Visited = visitedCopy,
+                        Score = score,
+                    });
                     return;
                 }
 
--- 2024/AdventOfCode2024/helpers/Helpers.cs
+++ 2024/AdventOfCode2024/helpers/Helpers.cs
@@ -13,17 +13,6 @@ public static class Helpers
 
     public static int ToInt(char c) => int.Parse(c.ToString());
 
-    public static bool AllSmallerThan(this IEnumerable<int> collection, int target)
-    {
-        return collection.All(x => x < target);
-    }
-
-    public static void AssertEqual<T>(T expected, T value)
-    {
-        if (!value.Equals(expected))
-            throw new Exception($"got: {value}, expected: {expected}");
-    }
-
     public static Direction CalculateDirection(Point from, Point to)
     {
         return (to - from) switch

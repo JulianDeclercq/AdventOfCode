95b7ff158aed11d0c2167b4c8e73a29952502977
Julian Declercq <julian.declercq1@gmail.com>
2025-10-13 21:57:17 +0300

    day 19 part 2 example

--- 2024/AdventOfCode2024/Day19.cs
+++ 2024/AdventOfCode2024/Day19.cs
@@ -12,6 +12,16 @@ public class Day19(string inputFilePath)
         return answer;
     }
 
+    public int Part2()
+    {
+        var lines = File.ReadAllLines(inputFilePath);
+        var patterns = lines[0].Split(", ").ToList();
+
+        var answer = lines.Skip(2).Sum(l => CountDesignPossibleWays(patterns, l));
+        Console.WriteLine(answer);
+        return answer;
+    }
+
     private static bool IsDesignPossible(IReadOnlyCollection<string> patterns, string design)
     {
         if (design.Length == 0)
@@ -19,5 +29,14 @@ public class Day19(string inputFilePath)
 
         var candidates = patterns.Where(design.StartsWith);
         return candidates.Any(candidate => IsDesignPossible(patterns, design[candidate.Length..]));
+    }
+
+    private static int CountDesignPossibleWays(IReadOnlyCollection<string> patterns, string design)
+    {
+        if (design.Length == 0)
+            return 1;
+
+        var candidates = patterns.Where(design.StartsWith);
+        return candidates.Sum(candidate => CountDesignPossibleWays(patterns, design[candidate.Length..]));
     }
 }
--- 2024/AdventOfCode2024/Program.cs
+++ 2024/AdventOfCode2024/Program.cs
@@ -4,7 +4,8 @@ var watch = System.Diagnostics.Stopwatch.StartNew();
 
 // var day = new Day19("input/example/day19e.txt");
 var day = new Day19("input/real/day19.txt");
-day.Part1();
+// day.Part1();
+day.Part2();
 
 watch.Stop();
 Console.WriteLine(watch.Elapsed);

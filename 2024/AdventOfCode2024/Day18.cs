63f5579dafef1ebc698549b5cb68e43b6a4b6aa9
Julian Declercq <julian.declercq1@gmail.com>
2025-10-20 19:24:08 +0200

    2024 day 18 part 1

--- 2024/AdventOfCode2024/Day18.cs
+++ 2024/AdventOfCode2024/Day18.cs
@@ -1,11 +1,9 @@
 ﻿using AdventOfCode2024.helpers;
-using Path = AdventOfCode2024.helpers.Path;
 
 namespace AdventOfCode2024;
 
 public class Day18(string inputPath)
 {
-    private record State(Point Position, Direction Direction);
 
     public void Solve()
     {
@@ -31,80 +29,49 @@ public class Day18(string inputPath)
             grid.Set(split[0], split[1], '#');
         }
 
-        var priorityQueue = new PriorityQueue<Path, int>();
-        var visited = new Dictionary<Point, int>(); // Only track position since turning is free
+        // Use simple BFS since all moves cost 1
+        var queue = new Queue<(Point position, int steps)>();
+        var visited = new HashSet<Point>();
 
         var start = new Point(0, 0);
         var end = new Point(width - 1, height - 1);
 
-        var initialPath = new Path
+        queue.Enqueue((start, 0));
+        visited.Add(start);
+
+        int? shortestPath = null;
+
+        while (queue.Count > 0)
         {
-            CurrentPosition = start,
-            CurrentDirection = Direction.East, // Direction doesn't matter for scoring
-            Score = 0, // We don't really need this, but keeping for compatibility
-            Visited = [start]
-        };
+            var (currentPos, currentSteps) = queue.Dequeue();
 
-        priorityQueue.Enqueue(initialPath, 0);
-        Path? bestPath = null;
-
-        while (priorityQueue.Count > 0)
-        {
-            var currentPath = priorityQueue.Dequeue();
-
-            // If we've reached the end, this is our answer (Dijkstra guarantees optimal)
-            if (currentPath.CurrentPosition.Equals(end))
+            // If we've reached the end, this is our answer (BFS guarantees optimal for unweighted graphs)
+            if (currentPos.Equals(end))
             {
-                bestPath = currentPath;
+                shortestPath = currentSteps;
                 break;
             }
 
-            var currentSteps = currentPath.Visited.Count - 1; // -1 because start doesn't count as a step
-
-            // Skip if we've already found a better path to this position
-            if (visited.TryGetValue(currentPath.CurrentPosition, out var prevSteps) && prevSteps < currentSteps)
-                continue;
-
-            visited[currentPath.CurrentPosition] = currentSteps;
-
             // Explore all valid neighbors
-            var neighbours = grid.NeighboursExtended(currentPath.CurrentPosition, includeDiagonals: false)
-                .Where(n => n.Value != '#') // Not a wall
+            var neighbours = grid.NeighboursExtended(currentPos, includeDiagonals: false)
+                .Where(n => n.Value != '#' && !visited.Contains(n.Position)) // Not a wall and not visited
                 .ToArray();
 
             foreach (var neighbour in neighbours)
             {
-                var newSteps = currentSteps + 1;
-
-                // Only add to queue if we haven't visited this position or found a better path
-                if (!visited.TryGetValue(neighbour.Position, out var existingSteps) || newSteps < existingSteps)
-                {
-                    var newPath = currentPath.Copy();
-                    newPath.CurrentPosition = Point.Copy(neighbour.Position);
-                    newPath.Visited.Add(neighbour.Position);
-
-                    // Direction doesn't matter for scoring, but we can set it for completeness
-                    var neighbourDirection =
-                        Helpers.CalculateCardinalDirection(currentPath.CurrentPosition, neighbour.Position);
-                    if (neighbourDirection != null)
-                        newPath.CurrentDirection = neighbourDirection.Value;
-
-                    priorityQueue.Enqueue(newPath, newSteps);
-                }
+                visited.Add(neighbour.Position);
+                queue.Enqueue((neighbour.Position, currentSteps + 1));
             }
         }
 
         // Print the result
-        if (bestPath != null)
+        if (shortestPath.HasValue)
         {
-            var steps = bestPath.Visited.Count - 1; // -1 because start doesn't count as a step
-            Console.WriteLine($"Shortest path found! Steps: {steps}");
+            Console.WriteLine($"Shortest path found! Steps: {shortestPath.Value}");
         }
         else
         {
             Console.WriteLine("No path found!");
         }
-        //
-        // Console.WriteLine(grid);
     }
 }

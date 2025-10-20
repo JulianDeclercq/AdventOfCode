120b2d3af470f79f0147ac225f646f909f9fe44e
Julian Declercq <julian.declercq1@gmail.com>
2025-10-20 19:09:07 +0200

    2024 day 18 parse grid

--- 2024/AdventOfCode2024/Day18.cs
+++ 2024/AdventOfCode2024/Day18.cs
@@ -1,8 +1,33 @@
-﻿namespace AdventOfCode2024;
+﻿using AdventOfCode2024.helpers;
+
+namespace AdventOfCode2024;
 
 public class Day18(string inputPath)
 {
     public void Solve()
     {
+        int width, height, bytesToTake;
+        if (inputPath.Contains("example"))
+        {
+            width = 7;
+            height = 7;
+            bytesToTake = 12;
+        }
+        else // real input
+        {
+            width = 71;
+            height = 71;
+            bytesToTake = 1024;
+        }
+
+        var input = File.ReadAllLines(inputPath);
+        var grid = new Grid<char>(width, height, Enumerable.Repeat('.', width * height), '@');
+        foreach (var line in input.Take(bytesToTake))
+        {
+            var split = line.Split(',').Select(int.Parse).ToArray();
+            grid.Set(split[0], split[1], '#');
+        }
+
+        Console.WriteLine(grid);
     }
 }

b863f9cdf3f66e276b284cc0e3e586b7c7637245
Julian Declercq <julian.declercq1@gmail.com>
2025-12-02 12:31:48 +0100

    day 2 part 1

--- 2025/day2.lua
+++ 2025/day2.lua
@@ -16,14 +16,10 @@ local function is_valid_id(id)
 	local first = tonumber(first_str)
 	local second_str = id:sub((#id / 2) + 1)
 	local second = tonumber(second_str)
-	-- print("id" .. id)
-	-- print("id_number " .. tonumber(id))
 
 	-- avoid problems with leading zeroes in second_str
 	if #first_str == #second_str then
-		-- numeric equality
 		if first == second then
-			-- print("match " .. first_str .. " " .. second_str)
 			return true
 		end
 	end
@@ -31,24 +27,22 @@ end
 
 local function part1()
 	-- local input = io.lines("example/day2.txt")
-	local input = io.lines("input/day2.txt")
-	for line in input do -- theres only one line so clean up this loop
-		local ranges = split(line, ",")
-		-- print(table.concat(ranges, "\n"))
-		local answer = 0
-		for _, range in ipairs(ranges) do
-			-- print(range)
-			local limits = split(range, "-")
-			local min, max = limits[1], limits[2]
-			for i = min, max, 1 do
-				local id = tostring(i)
-				if is_valid_id(id) then
-					answer = answer + tonumber(id)
-				end
+	local line = io.lines("input/day2.txt")() -- first line only
+	local ranges = split(line, ",")
+	-- print(table.concat(ranges, "\n"))
+	local answer = 0
+	for _, range in ipairs(ranges) do
+		-- print(range)
+		local limits = split(range, "-")
+		local min, max = limits[1], limits[2]
+		for i = min, max, 1 do
+			local id = tostring(i)
+			if is_valid_id(id) then
+				answer = answer + tonumber(id)
 			end
 		end
-		print(answer)
 	end
+	print(answer)
 end
 
 part1()

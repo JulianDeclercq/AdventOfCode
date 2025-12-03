d606ef20c4f5d8b73b4489707fdb728b2edab21c
Julian Declercq <julian.declercq1@gmail.com>
2025-12-03 11:35:59 +0100

    day 3 part 1

--- 2025/day3.lua
+++ 2025/day3.lua
@@ -1,10 +1,11 @@
 -- local lines = io.lines("example/day3.txt")
 local lines = io.lines("input/day3.txt")
 local answer = 0
-for battery in lines do
-	local highest, highest_idx, next_highest = -1, -1, -1
-	for i = 1, #battery - 1, 1 do -- ignore last time
-		local current = tonumber(battery:sub(i, i))
+
+local function find_highest(str, start_idx, end_idx)
+	local highest, highest_idx = -1, -1
+	for i = start_idx, end_idx, 1 do
+		local current = tonumber(str:sub(i, i))
 		if current == nil then
 			return
 		end
@@ -14,16 +15,12 @@ for battery in lines do
 			highest_idx = i
 		end
 	end
-	for i = highest_idx + 1, #battery, 1 do
-		local current = tonumber(battery:sub(i, i))
-		if current == nil then
-			return
-		end
+	return highest, highest_idx
+end
 
-		if current > next_highest then
-			next_highest = current
-		end
-	end
+for battery in lines do
+	local highest, highest_idx = find_highest(battery, 1, #battery - 1)
+	local next_highest = find_highest(battery, highest_idx + 1, #battery)
 	answer = answer + tonumber(highest .. next_highest)
 end
 

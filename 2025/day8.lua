7cee089abb96dd2ae10b83776d455f237a7117c0
Julian Declercq <julian.declercq1@gmail.com>
2025-12-08 23:55:47 +0100

    day 8 part 1 create distances table

--- 2025/day8.lua
+++ 2025/day8.lua
@@ -12,6 +12,15 @@ local function euclidean_distance(p1, p2)
 	return math.sqrt(math.pow(p1.x - p2.x, 2) + math.pow(p1.y - p2.y, 2) + math.pow(p1.z - p2.z, 2))
 end
 
+local function unique_key(p1, p2)
+	local sorted = { p1, p2 }
+	table.sort(sorted, function()
+		return p1.x < p2.x
+	end)
+	return inspect(sorted) -- use inspect as a general "to string"
+end
+
+local distances = {}
 local function part1()
 	for amount = 1, 10 do
 		local lowest_distance = 99999999999999
@@ -19,19 +28,20 @@ local function part1()
 		for i, outer in ipairs(boxes) do
 			for j, inner in ipairs(boxes) do
 				if inner ~= outer then
-					local dist = euclidean_distance(outer, inner)
-					if dist < lowest_distance then
-						lowest_distance = dist
-						lhs = inner
-						rhs = outer
+					local key = unique_key(outer, inner)
+					if distances[key] == nil then
+						local distance = euclidean_distance(outer, inner)
+						distances[key] = { p1 = outer, p2 = inner, distance = distance }
 					end
 				end
 			end
 		end
-
-		print(inspect(lhs))
-		print(inspect(rhs))
 	end
 end
 
 part1()
+
+local key = unique_key(boxes[1], boxes[2])
+-- print(key)
+-- print("lel")
+print(inspect(distances[key]))

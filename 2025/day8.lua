9382e36a4a592284d565dff7c24b47362fc3c837
Julian Declercq <julian.declercq1@gmail.com>
2025-12-09 10:21:59 +0100

    wip

--- 2025/day8.lua
+++ 2025/day8.lua
@@ -3,48 +3,55 @@ local helpers = require("helpers")
 local inspect = require("inspect")
 local lines = io.lines("example/day8.txt")
 local boxes = {}
+local circuits = {}
 for line in lines do
 	local split = helpers.split(line, ",")
-	table.insert(
-		boxes,
-		{ x = tonumber(split[1]), y = tonumber(split[2]), z = tonumber(split[3]), circuit = helpers.uuid() }
-	)
+	local circuit_id = helpers.uuid()
+	local box = { x = tonumber(split[1]), y = tonumber(split[2]), z = tonumber(split[3]), circuit = circuit_id }
+	table.insert(boxes, box)
+	circuits[circuit_id] = box
 end
 
 local function euclidean_distance(p1, p2)
 	return math.sqrt(math.pow(p1.x - p2.x, 2) + math.pow(p1.y - p2.y, 2) + math.pow(p1.z - p2.z, 2))
 end
 
+local function box_sorter(lhs, rhs)
+	if lhs.x ~= rhs.x then
+		return lhs.x < rhs.x
+	elseif lhs.y ~= rhs.y then
+		return lhs.y < rhs.y
+	else
+		return lhs.z < rhs.z
+	end
+end
+
+-- to ensure consistency in which gets added to which, sort and always pick the first one
+-- TODO: Explain better in comment
+local function circuit_candidate(p1, p2)
+	local sorted = { p1, p2 }
+	table.sort(sorted, box_sorter)
+	return sorted[1]
+end
+
 local function unique_key(p1, p2)
 	local sorted = { p1, p2 }
-	table.sort(sorted, function(lhs, rhs)
-		if lhs.x ~= rhs.x then
-			return lhs.x < rhs.x
-		elseif lhs.y ~= rhs.y then
-			return lhs.y < rhs.y
-		else
-			return lhs.z < rhs.z
-		end
-	end)
+	table.sort(sorted, box_sorter)
 	return inspect(sorted) -- use inspect as a general "to string"
 end
 
 local distances_lookup = {}
 local distances_list = {}
 local function part1()
-	for amount = 1, 10 do
-		local lowest_distance = 99999999999999
-		local lhs, rhs = {}, {}
-		for i, outer in ipairs(boxes) do
-			for j, inner in ipairs(boxes) do
-				if inner ~= outer then
-					local key = unique_key(outer, inner)
-					if distances_lookup[key] == nil then
-						local distance = euclidean_distance(outer, inner)
-						local box = { p1 = outer, p2 = inner, distance = distance }
-						distances_lookup[key] = box
-						table.insert(distances_list, box)
-					end
+	for i, outer in ipairs(boxes) do
+		for j, inner in ipairs(boxes) do
+			if inner ~= outer then
+				local key = unique_key(outer, inner)
+				if distances_lookup[key] == nil then
+					local distance = euclidean_distance(outer, inner)
+					local box = { p1 = outer, p2 = inner, distance = distance }
+					distances_lookup[key] = box
+					table.insert(distances_list, box)
 				end
 			end
 		end
@@ -56,11 +63,18 @@ local function part1()
 	end)
 
 	-- traverse the pairs and start making circuits
-	local circuits = {}
-	for _, dist in ipairs(distances_list) do
+	for i, dist in ipairs(distances_list) do
+		-- TODO: Check if the other box is already in a non-single circuit instead of always picking the candidate? not sure. need an example
+		-- TODO: What happens when both are already in a circuit? do both circuits get merged?
+
+		-- TODO: How am i going to update the circuit on all distances lmaoo I need references
 		print(inspect(dist))
-		-- break
+		break
 	end
+
+	-- for _, circuit in pairs(circuits) do
+	-- 	print(inspect(circuit))
+	-- end
 end
 
 part1()
